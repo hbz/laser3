4922af1f468dd4f2eb9af214355a52eb462adb31
David Klober <david.klober@hbz-nrw.de>
2017-10-13 14:40:28 +0200

    config

--- app/grails-app/conf/DataSource.groovy
+++ app/grails-app/conf/DataSource.groovy
@@ -7,8 +7,8 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-    //cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.SingletonEhCacheRegionFactory' // CAUTION: USE FOR DB-MIGRATION-PLUGIN
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    //cache.region.factory_class = 'net.sf.ehcache.hibernate.SingletonEhCacheRegionFactory' // CAUTION: USE FOR DB-MIGRATION-PLUGIN
 }
 // environment specific settings
 environments {
