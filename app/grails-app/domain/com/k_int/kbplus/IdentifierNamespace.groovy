d635ee1cfba757632fb1df44b9e8696ea09a3c8e
David Klober <david.klober@hbz-nrw.de>
2018-02-14 08:46:36 +0100

    refactoring identifier namespace

--- app/grails-app/domain/com/k_int/kbplus/IdentifierNamespace.groovy
+++ app/grails-app/domain/com/k_int/kbplus/IdentifierNamespace.groovy
@@ -3,7 +3,7 @@ package com.k_int.kbplus
 class IdentifierNamespace {
 
   String ns
-  RefdataValue nstype
+  RefdataValue nsType
   Boolean hide
   String validationRegex
   String family
@@ -12,18 +12,18 @@ class IdentifierNamespace {
   static mapping = {
     id column:'idns_id'
     ns column:'idns_ns'
-    nstype column:'idns_type_fl'
+    nsType column:'idns_type_fk'
     hide column:'idns_hide'
     validationRegex column:'idns_val_regex'
     family column:'idns_family'
-    nonUnique column:'idns_nonUnique'
+    nonUnique column:'idns_non_unique'
   }
 
   static constraints = {
-    nstype nullable:true, blank:false
+    nsType nullable:true, blank:false
     hide nullable:true, blank:false
-    validationRegex nullable:true, black:false
-    family nullable:true, black:false
-    nonUnique nullable:true, black:false
+    validationRegex nullable:true, blank:false
+    family          nullable:true, blank:false
+    nonUnique       nullable:true, blank:false
   }
 }
