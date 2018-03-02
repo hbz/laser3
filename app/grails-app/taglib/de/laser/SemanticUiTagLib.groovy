c8d5ab1b1a4d4cad21d648dbe89126890d1f8f4d
Ingrid Bluoss <ingrid.bluoss@hbz-nrw.de>
2018-03-02 12:33:43 +0100

    new colors

--- app/grails-app/taglib/de/laser/SemanticUiTagLib.groovy
+++ app/grails-app/taglib/de/laser/SemanticUiTagLib.groovy
@@ -166,7 +166,7 @@ class SemanticUiTagLib {
 
     def filter = { attrs, body ->
 
-        out << '<div class="ui yellow segment">'
+        out << '<div class="ui la-filter segment">'
         out <<   body()
         out << '</div>'
     }
