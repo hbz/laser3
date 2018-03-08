5dea6a3e6ec9542bb197c40d00692af6304784a8
Ingrid Bluoss <ingrid.bluoss@hbz-nrw.de>
2018-03-08 15:38:12 +0100

    the magic is better

--- app/grails-app/taglib/de/laser/SemanticUiDropdownTagLib.groovy
+++ app/grails-app/taglib/de/laser/SemanticUiDropdownTagLib.groovy
@@ -64,7 +64,7 @@ class SemanticUiDropdownTagLib {
     def actionsDropdown = { attrs, body ->
 
         out << '<div class="ui simple dropdown button">'
-        out <<  '<i class="play icon"></i>'
+        out <<  '<i class="magic icon"></i>'
         out <<  '<div class="menu">'
 
         out <<          body()
