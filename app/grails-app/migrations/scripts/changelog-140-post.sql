fddcaa8802989b9d2582695c31b0816fb7c5e3d7
David Klober <david.klober@hbz-nrw.de>
2018-03-13 15:59:39 +0100

    db clean up script

--- app/grails-app/migrations/scripts/changelog-140-post.sql
+++ app/grails-app/migrations/scripts/changelog-140-post.sql
@@ -7,3 +7,8 @@ DELETE FROM role WHERE authority in ('KBPLUS_EDITOR', 'ROLE_EDITOR');
 /* remove user based roles in global context */
 
 DELETE FROM user_role WHERE role_id in (SELECT id FROM role WHERE authority in ('INST_USER', 'INST_ADM'));
+
+/* remove legacy person roles; now splitted into function and responsibility */
+
+DELETE FROM refdata_value WHERE rdv_owner IN (SELECT rdc_id FROM refdata_category WHERE rdc_description = 'Person Role');
+DELETE FROM refdata_category WHERE rdc_description = 'Person Role';
