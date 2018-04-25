fda71f24dcaec343a8a17045b431c6c981964012
Moetez Djebeniani <moetez.djebeniani@hbz-nrw.de>
2018-04-25 15:02:35 +0200

    Modifing BatchJobs

--- app/grails-app/jobs/com/k_int/kbplus/batch/GlobalDataSyncJob.groovy
+++ app/grails-app/jobs/com/k_int/kbplus/batch/GlobalDataSyncJob.groovy
@@ -10,7 +10,7 @@ class GlobalDataSyncJob {
     // Cron:: Min Hour DayOfMonth Month DayOfWeek Year
     // Example - every 10 mins 0 0/10 * * * ? 
     // At 5 past 4am every day
-    cron name:'globalDataSyncTrigger', startDelay:180000, cronExpression: "0 5 4 * * ?"
+    cron name:'globalDataSyncTrigger', startDelay:180000, cronExpression: "0 1 0 * * ?"
     // cronExpression: "s m h D M W Y"
     //                  | | | | | | `- Year [optional]
     //                  | | | | | `- Day of Week, 1-7 or SUN-SAT, ?
