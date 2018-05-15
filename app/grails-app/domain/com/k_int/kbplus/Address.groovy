2890de92b64ab094ffe7f29465d3762ececebe80
David Klober <david.klober@hbz-nrw.de>
2018-05-15 12:37:39 +0200

    reworked makeshift xml import for organisations
    (cherry picked from commit 5a3bbb38e9129a804ec65746037d6da720aa7f55)

--- app/grails-app/domain/com/k_int/kbplus/Address.groovy
+++ app/grails-app/domain/com/k_int/kbplus/Address.groovy
@@ -10,17 +10,26 @@ import groovy.util.logging.*
 
 @Log4j
 class Address {
-    
+
     String street_1
     String street_2
-    String pob
     String zipcode
     String city
+
+    String pob
+    String pobZipcode
+    String pobCity
+
     RefdataValue state      // RefdataCategory 'Federal State'
     RefdataValue country    // RefdataCategory 'Country'
     RefdataValue type       // RefdataCategory 'AddressType'
-    Person prs          // person related contact; exclusive with org
-    Org    org          // org related contact; exclusive with prs
+
+    String name
+    String additionFirst
+    String additionSecond
+
+    Person prs              // person related contact; exclusive with org
+    Org    org              // org related contact; exclusive with prs
     
     static mapping = {
         id       column:'adr_id'
@@ -28,23 +37,33 @@ class Address {
         street_1 column:'adr_street_1'
         street_2 column:'adr_street_2'
         pob      column:'adr_pob'
+        pobZipcode   column:'adr_pob_zipcode'
+        pobCity      column:'adr_pob_city'
         zipcode  column:'adr_zipcode'
         city     column:'adr_city'
         state    column:'adr_state_rv_fk'
         country  column:'adr_country_rv_fk'
+        name     column:'adr_name'
+        additionFirst   column:'adr_addition_first'
+        additionSecond  column:'adr_addition_second'
         type     column:'adr_type_rv_fk'
         prs      column:'adr_prs_fk'
         org      column:'adr_org_fk'
     }
     
     static constraints = {
-        street_1 (nullable:false, blank:false)
-        street_2 (nullable:true,  blank:true)
-        pob      (nullable:true,  blank:true)
-        zipcode  (nullable:false, blank:false)
-        city     (nullable:false, blank:false)
-        state    (nullable:true,  blank:true)
-        country  (nullable:true,  blank:true)
+        street_1 (nullable:true,  blank:false)
+        street_2 (nullable:true,  blank:false)
+        pob         (nullable:true,  blank:false)
+        pobZipcode  (nullable:true,  blank:false)
+        pobCity     (nullable:true,  blank:false)
+        zipcode  (nullable:true, blank:false)
+        city     (nullable:true, blank:false)
+        state    (nullable:true,  blank:false)
+        country  (nullable:true,  blank:false)
+        name            (nullable:true,  blank:false)
+        additionFirst   (nullable:true,  blank:false)
+        additionSecond  (nullable:true,  blank:false)
         type     (nullable:false)
         prs      (nullable:true)
         org      (nullable:true)
@@ -59,17 +78,33 @@ class Address {
         zipcode + ' ' + city + ', ' + street_1 + ' ' + street_2 + ' (' + id + '); ' + type?.value
     }
 
-    static def lookup(street1, street2, postbox, zipcode, city, state, country, type, person, organisation) {
+    static def lookup(
+            name,
+            street1,
+            street2,
+            zipcode,
+            city,
+            state,
+            country,
+            postbox,
+            pobZipcode,
+            pobCity,
+            type,
+            person,
+            organisation) {
 
         def address
         def check = Address.findAllWhere(
+                name:           name ?: null,
                 street_1: street1 ?: null,
                 street_2: street2 ?: null,
-                pob:      postbox ?: null,
                 zipcode:  zipcode ?: null,
                 city:     city ?: null,
                 state:    state ?: null,
                 country:  country ?: null,
+                pob:            postbox ?: null,
+                pobZipcode:     pobZipcode ?: null,
+                pobCity:        pobCity ?: null,
                 type:     type ?: null,
                 prs:      person,
                 org:      organisation
@@ -81,29 +116,45 @@ class Address {
         address
     }
 
-    static def lookupOrCreate(street1, street2, postbox, zipcode, city, state, country, type, person, organisation) {
+    static def lookupOrCreate(
+            name,
+            street1,
+            street2,
+            zipcode,
+            city,
+            state,
+            country,
+            postbox,
+            pobZipcode,
+            pobCity,
+            type,
+            person,
+            organisation) {
         
-        def info   = "saving new address: ${type}}"
+        def info   = "saving new address: ${type}"
         def result = null
         
         if (person && organisation) {
             type = RefdataValue.findByValue("Job-related")
         }
 
-        def check = Address.lookup(street1, street2, postbox, zipcode, city, state, country, type, person, organisation)
+        def check = Address.lookup(name, street1, street2, zipcode, city, state, country, postbox, pobZipcode, pobCity, type, person, organisation)
         if (check) {
             result = check
             info += " > ignored; duplicate found"
         }
         else {
             result = new Address(
+                name:     name,
                 street_1: street1,
                 street_2: street2,
-                pob:      postbox,
                 zipcode:  zipcode,
                 city:     city,
                 state:    state,
                 country:  country,
+                pob:      postbox,
+                pobZipcode: pobZipcode,
+                pobCity:  pobCity,
                 type:     type,
                 prs:      person,
                 org:      organisation
--- app/grails-app/views/templates/cpa/_address.gsp
+++ app/grails-app/views/templates/cpa/_address.gsp
@@ -3,24 +3,39 @@
 		<g:link controller="address" action="show" id="${address?.id}">
             ${address.type?.getI10n('value')}:
 
-			<br />
-			${address?.street_1.encodeAsHTML()}
-			${address?.street_2.encodeAsHTML()}
-
-			<g:if test="${address?.pob}">
+            <g:if test="${address?.name}">
                 <br />
-                ${address?.pob.encodeAsHTML()}
+                ${address?.name.encodeAsHTML()}
             </g:if>
 
+			<br />
+			${address?.street_1.encodeAsHTML()} ${address?.street_2.encodeAsHTML()}
+
             <br />
-            ${address?.zipcode.encodeAsHTML()}
-            ${address?.city.encodeAsHTML()}
+            ${address?.zipcode.encodeAsHTML()} ${address?.city.encodeAsHTML()}
 
 			<g:if test="${address?.state || address?.country}">
                 <br />
                 ${address?.state?.getI10n('value')}
                 <g:if test="${address?.state && address?.country}">, </g:if>
                 ${address?.country.getI10n('value')}
+            </g:if>
+
+            <g:if test="${address?.pob}">
+                <br />
+                <g:message code="address.pob.label" default="Pob" />
+                ${address?.pob.encodeAsHTML()}
+                <g:if test="${address?.pobZipcode || address?.pobCity}">, </g:if>
+                ${address?.pobZipcode?.encodeAsHTML()} ${address?.pobCity?.encodeAsHTML()}
+            </g:if>
+
+            <g:if test="${address?.additionFirst}">
+                <br />
+                ${address?.additionFirst.encodeAsHTML()}
+            </g:if>
+            <g:if test="${address?.additionSecond}">
+                <br />
+                ${address?.additionSecond.encodeAsHTML()}
             </g:if>
 		</g:link>
 	</div>
