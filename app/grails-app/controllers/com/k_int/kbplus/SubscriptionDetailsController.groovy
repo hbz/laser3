aad41fd5581b4f33f38e2edb72c186be528b3a57
David Klober <david.klober@hbz-nrw.de>
2018-08-08 15:43:12 +0200

    bugfix: erms-464

--- app/grails-app/controllers/com/k_int/kbplus/SubscriptionDetailsController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/SubscriptionDetailsController.groovy
@@ -795,7 +795,7 @@ class SubscriptionDetailsController {
                         def cons_sub = new Subscription(
                                 type: result.subscriptionInstance.type?:"",
                                 status: subStatus,
-                                name: result.subscriptionInstance.name + " (${postfix})",
+                                name: result.subscriptionInstance.name + " (" + (cm.get(0).shortname ?: cm.get(0).name) + ")",
                                 startDate: result.subscriptionInstance.startDate,
                                 endDate: result.subscriptionInstance.endDate,
                                 manualRenewalDate: result.subscriptionInstance.manualRenewalDate,
