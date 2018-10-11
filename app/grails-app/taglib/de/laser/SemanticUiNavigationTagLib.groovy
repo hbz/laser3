2160f64741bb3b0269a4df8f0e5c8091914d7637
Ingrid Bluoss <ingrid.bluoss@hbz-nrw.de>
2018-10-11 13:57:50 +0200

    improvement

--- app/grails-app/taglib/de/laser/SemanticUiNavigationTagLib.groovy
+++ app/grails-app/taglib/de/laser/SemanticUiNavigationTagLib.groovy
@@ -18,7 +18,7 @@ class SemanticUiNavigationTagLib {
 
     def breadcrumbs = { attrs, body ->
 
-        out <<   '<div class="ui small breadcrumb">'
+        out <<   '<div class="ui breadcrumb">'
         out <<     crumb([controller: 'home', text:'<i class="home icon"></i>'])
         out <<     body()
         out <<   '</div>'
