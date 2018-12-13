c80627da8987ef21c41caa8b94d5d8d2911c93a0
David Klober <david.klober@hbz-nrw.de>
2018-12-13 11:25:08 +0100

    adopting rc-0.12

--- app/grails-app/migrations/changelog.groovy
+++ app/grails-app/migrations/changelog.groovy
@@ -4,53 +4,44 @@ databaseChangeLog = {
 		// TODO add changes and preconditions here
 	}
 
+    /*
     // << grails prod dbm-changelog-sync
-
     // release 0.2
     include file: 'changelog-100.groovy'
-
 	// release 0.3
 	include file: 'changelog-110.groovy'
 	include file: 'changelog-120.groovy'
-
 	// release 0.3.1
 	include file: 'changelog-130.groovy'
-
 	// release 0.4
 	include file: 'changelog-140.groovy'
 	include file: 'changelog-150.groovy'
-
 	// release 0.4.5 & 0.5
 	include file: 'changelog-160.groovy'
 	include file: 'changelog-165.groovy'
-
 	// release 0.6
 	include file: 'changelog-170.groovy'
-
 	// release 0.7
 	include file: 'changelog-180.groovy'
 	include file: 'changelog-185.groovy'
 	include file: 'changelog-190.groovy'
-
 	// release 0.8
 	include file: 'changelog-200.groovy'
-
 	// release 0.9
 	include file: 'changelog-210.groovy'
 	include file: 'changelog-215.groovy'
-
 	// release 0.10
 	include file: 'changelog-220.groovy'
 	include file: 'changelog-225.groovy'
-
 	// release 0.11
 	include file: 'changelog-230.groovy'
-
 	// release 0.11.2
 	include file: 'changelog-240.groovy'
-
 	// release 0.12
 	include file: 'changelog-250.groovy'
+    */
 
     // postgresql migration; first diff
+
+	//include file: 'changelog-0.groovy'
 }
--- app/grails-app/migrations/toPostgresql/TODO.md
+++ app/grails-app/migrations/toPostgresql/TODO.md
@@ -4,13 +4,14 @@
 - copy current MySQL database
 - use dataCleanupMySQL.sql
 - use exportFilesMySQL.sql
-- create PostgreSQL database
+- create PostgreSQL database (set search_path to public)
+- init database tables (starting app)
 - migrate data with pgLoader using migrate2postgresql.load
 - use dataCleanupPostgreSQL.sql
-- reset database migration state
+- init database migration
 
 
-    grails prod dbm-create-changelog --defaultSchema=public
+    //grails prod dbm-create-changelog --defaultSchema=public
     grails prod dbm-changelog-sync --defaultSchema=public
     grails prod dbm-gorm-diff --defaultSchema=public --add changelog-0.groovy
 
