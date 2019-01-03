e430dc33c8cf1aa969b05656efd7ce63eb5baa22
David Klober <david.klober@hbz-nrw.de>
2019-01-03 12:04:43 +0100

    changed cron job triggers

--- app/grails-app/jobs/com/k_int/kbplus/batch/AdminJob.groovy
+++ app/grails-app/jobs/com/k_int/kbplus/batch/AdminJob.groovy
@@ -11,7 +11,7 @@ class AdminJob extends AbstractJob {
         // Cron:: Min Hour DayOfMonth Month DayOfWeek Year
         // Example - every 10 mins 0 0/10 * * * ?
         // At 5 past 2am on the first of every month - Sync stats
-        cron name:'AdminTrigger', cronExpression: "30 0 7 * * ?"
+        cron name:'AdminTrigger', cronExpression: "0 0 7 * * ?"
         // cronExpression: "s m h D M W Y"
         //                  | | | | | | `- Year [optional]
         //                  | | | | | `- Day of Week, 1-7 or SUN-SAT, ?
