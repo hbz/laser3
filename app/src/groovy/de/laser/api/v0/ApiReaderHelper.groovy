dd786973c7d03a4eca232dbd0babc41f24518ee5
David Klober <david.klober@hbz-nrw.de>
2019-01-22 12:57:05 +0100

    erms-919 - new list endpoints - api 0.34

--- app/src/groovy/de/laser/api/v0/ApiReaderHelper.groovy
+++ app/src/groovy/de/laser/api/v0/ApiReaderHelper.groovy
@@ -1,6 +1,7 @@
 package de.laser.api.v0
 
 import com.k_int.kbplus.*
+import de.laser.api.v0.entities.*
 import de.laser.helper.Constants
 import groovy.util.logging.Log4j
 
@@ -112,19 +113,20 @@ class ApiReaderHelper {
     }
 
     static resolveLicenseStub(License lic, Org context) {
+        resolveLicenseStub(lic, context, false)
+    }
+
+    static resolveLicenseStub(License lic, Org context, hasAccess) {
         def result = [:]
-        def hasAccess = false
 
         if (!lic) {
             return null
         }
 
-        lic.getOrgLinks().each { orgRole ->
-            // TODO check orgRole.roleType
-            if (orgRole.getOrg().id == context?.id) {
-                hasAccess = true
-            }
+        if (! hasAccess) {
+            hasAccess = ApiLicense.calculateAccess(lic, context, hasAccess)
         }
+
         if (hasAccess) {
             result.globalUID    = lic.globalUID
             result.impId        = lic.impId
@@ -200,19 +202,20 @@ class ApiReaderHelper {
      * @return MAP | Constants.HTTP_FORBIDDEN
      */
     static resolveSubscriptionStub(Subscription sub, Org context) {
+        resolveSubscriptionStub(sub, context, false)
+    }
+
+    static resolveSubscriptionStub(Subscription sub, Org context, boolean hasAccess) {
         def result = [:]
-        def hasAccess = false
 
         if (!sub) {
             return null
         }
 
-        sub.getOrgRelations().each { orgRole ->
-            // TODO check orgRole.roleType
-            if (orgRole.getOrg().id == context?.id) {
-                hasAccess = true
-            }
+        if (! hasAccess) {
+            hasAccess = ApiSubscription.calculateAccess(sub, context, hasAccess)
         }
+
         if (hasAccess) {
             result.globalUID    = sub.globalUID
             result.name         = sub.name
