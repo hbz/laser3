fe344dc5f980a176da71e02a9ffca8247343c639
David Klober <david.klober@hbz-nrw.de>
2019-01-28 15:31:28 +0100

    minor data consistency check improvements

--- app/grails-app/services/de/laser/DataConsistencyService.groovy
+++ app/grails-app/services/de/laser/DataConsistencyService.groovy
@@ -8,30 +8,30 @@ class DataConsistencyService {
 
     def checkImportIds() {
         Map result = [
-                orgs: [],
-                packages: [],
-                platforms: [],
-                titleInstances: [],
-                tipps: []
+                Orgs: [],
+                Packages: [],
+                Platforms: [],
+                TitleInstances: [],
+                Tipps: []
         ]
 
-        result.orgs = Org.executeQuery(
+        result.Orgs = Org.executeQuery(
                 'SELECT org.impId as impId, count(org.impId) as cnt FROM Org org GROUP BY org.impId ORDER BY org.impId'
         ).findAll{ it -> it[1] > 1}
 
-        result.packages = Package.executeQuery(
+        result.Packages = Package.executeQuery(
                 'SELECT pkg.impId as impId, count(pkg.impId) as cnt FROM Package pkg GROUP BY pkg.impId ORDER BY pkg.impId'
         ).findAll{ it -> it[1] > 1}
 
-        result.platforms = Platform.executeQuery(
+        result.Platforms = Platform.executeQuery(
                 'SELECT pf.impId as impId, count(pf.impId) as cnt FROM Platform pf GROUP BY pf.impId ORDER BY pf.impId'
         ).findAll{ it -> it[1] > 1}
 
-        result.titleInstances = TitleInstance.executeQuery(
+        result.TitleInstances = TitleInstance.executeQuery(
                 'SELECT ti.impId as impId, count(ti.impId) as cnt FROM TitleInstance ti GROUP BY ti.impId ORDER BY ti.impId'
         ).findAll{ it -> it[1] > 1}
 
-        result.tipps = TitleInstancePackagePlatform.executeQuery(
+        result.Tipps = TitleInstancePackagePlatform.executeQuery(
                 'SELECT tipp.impId as impId, count(tipp.impId) as cnt FROM TitleInstancePackagePlatform tipp GROUP BY tipp.impId ORDER BY tipp.impId'
         ).findAll{ it -> it[1] > 1}
 
@@ -40,52 +40,52 @@ class DataConsistencyService {
 
     def checkTitles() {
         Map result = [
-                orgs: [:],
-                packages: [:],
-                platforms: [:],
-                titleInstances: [:],
-                tipps: [:]
+                Orgs: [:],
+                Packages: [:],
+                Platforms: [:]
+                // TitleInstances: [:],
+                // Tipps: [:]
         ]
 
         // Orgs
 
-        result.orgs.name = Org.executeQuery(
+        result.Orgs.name = Org.executeQuery(
                 'SELECT org.name as name, count(org.name) as cnt FROM Org org GROUP BY org.name ORDER BY org.name'
         ).findAll{ it -> it[1] > 1}
 
-        result.orgs.shortname = Org.executeQuery(
+        result.Orgs.shortname = Org.executeQuery(
                 'SELECT org.shortname as shortname, count(org.shortname) as cnt FROM Org org GROUP BY org.shortname ORDER BY org.shortname'
         ).findAll{ it -> it[1] > 1}
 
-        result.orgs.shortcode = Org.executeQuery(
+        result.Orgs.shortcode = Org.executeQuery(
                 'SELECT org.shortcode as shortcode, count(org.shortcode) as cnt FROM Org org GROUP BY org.shortcode ORDER BY org.shortcode'
         ).findAll{ it -> it[1] > 1}
 
-        result.orgs.sortname = Org.executeQuery(
+        result.Orgs.sortname = Org.executeQuery(
                 'SELECT org.sortname as sortname, count(org.sortname) as cnt FROM Org org GROUP BY org.sortname ORDER BY org.sortname'
         ).findAll{ it -> it[1] > 1}
 
         // Packages
 
-        result.packages.name = Package.executeQuery(
+        result.Packages.name = Package.executeQuery(
                 'SELECT pkg.name as name, count(pkg.name) as cnt FROM Package pkg GROUP BY pkg.name ORDER BY pkg.name'
         ).findAll{ it -> it[1] > 1}
 
-        result.packages.sortName = Package.executeQuery(
+        result.Packages.sortName = Package.executeQuery(
                 'SELECT pkg.sortName as sortName, count(pkg.sortName) as cnt FROM Package pkg GROUP BY pkg.sortName ORDER BY pkg.sortName'
         ).findAll{ it -> it[1] > 1}
 
         // Platforms
 
-        result.platforms.name = Platform.executeQuery(
+        result.Platforms.name = Platform.executeQuery(
                 'SELECT pf.name as name, count(pf.name) as cnt FROM Platform pf GROUP BY pf.name ORDER BY pf.name'
         ).findAll{ it -> it[1] > 1}
 
-        result.platforms.normname = Platform.executeQuery(
+        result.Platforms.normname = Platform.executeQuery(
                 'SELECT pf.normname as normname, count(pf.normname) as cnt FROM Platform pf GROUP BY pf.normname ORDER BY pf.normname'
         ).findAll{ it -> it[1] > 1}
         /*
-        result.platforms.primaryUrl = Platform.executeQuery(
+        result.Platforms.primaryUrl = Platform.executeQuery(
                 'SELECT pf.primaryUrl as primaryUrl, count(pf.primaryUrl) as cnt FROM Platform pf GROUP BY pf.primaryUrl ORDER BY pf.primaryUrl'
         ).findAll{ it -> it[1] > 1}
         */
@@ -93,29 +93,30 @@ class DataConsistencyService {
         // TitleInstances
 
         /*
-        result.titleInstances.title = TitleInstance.executeQuery(
+        result.TitleInstances.title = TitleInstance.executeQuery(
                 'SELECT ti.title as title, count(ti.title) as cnt FROM TitleInstance ti GROUP BY ti.title ORDER By ti.title'
         ).findAll{ it -> it[1] > 1}
 
-        result.titleInstances.normTitle = TitleInstance.executeQuery(
+        result.TitleInstances.normTitle = TitleInstance.executeQuery(
                 'SELECT ti.normTitle as normTitle, count(ti.normTitle) as cnt FROM TitleInstance ti GROUP BY ti.normTitle ORDER BY ti.normTitle'
         ).findAll{ it -> it[1] > 1}
 
-        result.titleInstances.keyTitle = TitleInstance.executeQuery(
+        result.TitleInstances.keyTitle = TitleInstance.executeQuery(
                 'SELECT ti.keyTitle as keyTitle, count(ti.keyTitle) as cnt FROM TitleInstance ti GROUP BY ti.keyTitle ORDER BY ti.keyTitle'
         ).findAll{ it -> it[1] > 1}
 
-        result.titleInstances.sortTitle = TitleInstance.executeQuery(
+        result.TitleInstances.sortTitle = TitleInstance.executeQuery(
                 'SELECT ti.sortTitle as sortTitle, count(ti.sortTitle) as cnt FROM TitleInstance ti GROUP BY ti.sortTitle ORDER BY ti.sortTitle'
         ).findAll{ it -> it[1] > 1}
         */
 
         // TitleInstancePackagePlatforms
         /*
-        result.tipps.hostPlatformURL = TitleInstancePackagePlatform.executeQuery(
+        result.Tipps.hostPlatformURL = TitleInstancePackagePlatform.executeQuery(
                 'SELECT tipp.hostPlatformURL as hostPlatformURL, count(tipp.hostPlatformURL) as cnt FROM TitleInstancePackagePlatform tipp GROUP BY tipp.hostPlatformURL ORDER BY tipp.hostPlatformURL'
         ).findAll{ it -> it[1] > 1}
         */
+        
         result
     }
 }
