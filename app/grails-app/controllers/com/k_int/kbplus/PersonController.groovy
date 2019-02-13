ff7a768df01e6a692e749a0ee0a28a002d591b84
agalffy <andrisgalffy@gmail.com>
2019-02-13 16:24:17 +0100

    bug-erms-951
    as of ERMS-951, the display of a priavte contact has been limited to the context org. ROLE_ADMIN may access any contacts until further notice

--- app/grails-app/controllers/com/k_int/kbplus/PersonController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/PersonController.groovy
@@ -2,6 +2,7 @@ package com.k_int.kbplus
 
 import de.laser.controller.AbstractDebugController
 import de.laser.helper.DebugAnnotation
+import grails.plugin.springsecurity.SpringSecurityService
 import grails.plugin.springsecurity.annotation.Secured
 import grails.plugin.springsecurity.SpringSecurityUtils
 import org.springframework.dao.DataIntegrityViolationException
@@ -67,6 +68,12 @@ class PersonController extends AbstractDebugController {
             //redirect action: 'list'
             redirect(url: request.getHeader('referer'))
             return
+        }
+        else if(personInstance) {
+            if(contextService.org != personInstance.tenant && !SpringSecurityUtils.ifAnyGranted('ROLE_ADMIN')) {
+                redirect action: 'list'
+                return
+            }
         }
 
         def result = [
