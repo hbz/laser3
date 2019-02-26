ffbd93001191e5dde630987540c91f584de245be
David Klober <david.klober@hbz-nrw.de>
2019-02-26 16:01:31 +0100

    .

--- app/grails-app/migrations/manually/todo-20-30.sql
+++ app/grails-app/migrations/manually/todo-20-30.sql
@@ -1,7 +1,7 @@
 ﻿
 -- erms-1037
 -- 2019-02-26
--- execute before startup / after applying changelog-30.groovy
+-- execute before startup / local dev environment only
 ALTER TABLE public.refdata_category DROP COLUMN rdc_soft_data;
 ALTER TABLE public.refdata_value DROP COLUMN rdv_soft_data;
 
