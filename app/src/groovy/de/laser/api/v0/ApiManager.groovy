99ed9a5e069afcd2c881a9125929339f1b5b8b86
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2019-04-08 12:42:00 +0200

    API Changes

--- app/src/groovy/de/laser/api/v0/ApiManager.groovy
+++ app/src/groovy/de/laser/api/v0/ApiManager.groovy
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest
 @Log4j
 class ApiManager {
 
-    static final VERSION = '0.40'
+    static final VERSION = '0.41'
     static final NOT_SUPPORTED = false
 
     static final API_LEVEL_READ         = 'API_LEVEL_READ'
