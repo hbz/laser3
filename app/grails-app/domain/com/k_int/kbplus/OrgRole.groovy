890272b32a1d1d3ed853bcf637ac399ba724ef37
David Klober <david.klober@hbz-nrw.de>
2019-04-09 09:49:05 +0200

    reswitch to hibernate 4

--- app/grails-app/domain/com/k_int/kbplus/OrgRole.groovy
+++ app/grails-app/domain/com/k_int/kbplus/OrgRole.groovy
@@ -2,7 +2,8 @@ package com.k_int.kbplus
 
 import de.laser.helper.RefdataAnnotation
 import de.laser.traits.ShareableTrait
-import org.hibernate.event.PostUpdateEvent
+//import org.hibernate.event.PostUpdateEvent // Hibernate 3
+import org.hibernate.event.spi.PostUpdateEvent // Hibernate 4
 
 import javax.persistence.Column
 import javax.persistence.Transient
