5902718778b24359e842703763429bc11845c73f
David Klober <david.klober@hbz-nrw.de>
2019-05-24 10:48:44 +0200

    api 0.49

--- app/src/groovy/de/laser/api/v0/ApiManager.groovy
+++ app/src/groovy/de/laser/api/v0/ApiManager.groovy
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletRequest
 @Log4j
 class ApiManager {
 
-    static final VERSION = '0.48'
+    static final VERSION = '0.49'
     static final NOT_SUPPORTED = false
 
     static final API_LEVEL_READ         = 'API_LEVEL_READ'
