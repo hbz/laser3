82869430953326030bb55583fdc8db8a8b29bb1d
David Klober <david.klober@hbz-nrw.de>
2019-06-28 13:46:55 +0200

    added online counter

--- app/grails-app/services/de/laser/YodaService.groovy
+++ app/grails-app/services/de/laser/YodaService.groovy
@@ -1,13 +1,16 @@
 package de.laser
 
+import com.k_int.kbplus.auth.User
 import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.util.Holders
 import groovy.transform.CompileStatic
 import org.codehaus.groovy.grails.commons.GrailsApplication
 
-@CompileStatic
+//@CompileStatic
 class YodaService {
 
     GrailsApplication grailsApplication
+    def sessionRegistry = Holders.grailsApplication.mainContext.getBean('sessionRegistry')
 
     // gsp:
     // grailsApplication.mainContext.getBean("yodaService")
@@ -16,5 +19,9 @@ class YodaService {
     boolean showDebugInfo() {
         //enhanced as of ERMS-829
         return ( SpringSecurityUtils.ifAnyGranted('ROLE_ADMIN,ROLE_YODA,ROLE_DATAMANAGER') || grailsApplication.config.showDebugInfo )
+    }
+
+    int getNumberOfActiveUsers() {
+        (sessionRegistry.getAllPrincipals()).size()
     }
 }
