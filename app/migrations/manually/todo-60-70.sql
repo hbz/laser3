8369dd59b681fdc177c30197b81b9e9aaa68e100
David Klober <david.klober@hbz-nrw.de>
2019-07-04 10:31:04 +0200

    activated dbm changelog-76

--- app/migrations/manually/todo-60-70.sql
+++ app/migrations/manually/todo-60-70.sql
@@ -68,24 +68,24 @@
 --OLD: delete from refdata_value where rdv_owner = 54;
 --OLD: delete from refdata_category where rdc_id = 54;
 
-update issue_entitlement set ie_status_rv_fk = (
-    SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'TIPP Status' and v.rdv_value = 'Current' -- 369
-) where ie_status_rv_fk in (
-    SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'Entitlement Issue Status' and (
-                v.rdv_value = 'Live' or v.rdv_value = 'Current'-- 296 or 808
-        )
-);
-
-update issue_entitlement set ie_status_rv_fk = (
-    SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'TIPP Status' and v.rdv_value = 'Deleted' -- 369
-) where ie_status_rv_fk = (
-    SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'Entitlement Issue Status' and v.rdv_value = 'Deleted' -- 296
-);
-
-delete from refdata_value where rdv_owner = (
-    SELECT rdc_id FROM refdata_category WHERE rdc_description = 'Entitlement Issue Status'
-);
-
-delete from refdata_category where rdc_id = (
-    SELECT rdc_id FROM refdata_category WHERE rdc_description = 'Entitlement Issue Status'
-);
+-- update issue_entitlement set ie_status_rv_fk = (
+--     SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'TIPP Status' and v.rdv_value = 'Current' -- 369
+-- ) where ie_status_rv_fk in (
+--     SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'Entitlement Issue Status' and (
+--                 v.rdv_value = 'Live' or v.rdv_value = 'Current'-- 296 or 808
+--         )
+-- );
+--
+-- update issue_entitlement set ie_status_rv_fk = (
+--     SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'TIPP Status' and v.rdv_value = 'Deleted' -- 369
+-- ) where ie_status_rv_fk = (
+--     SELECT v.rdv_id FROM refdata_value v JOIN refdata_category c ON (v.rdv_owner = c.rdc_id) WHERE c.rdc_description = 'Entitlement Issue Status' and v.rdv_value = 'Deleted' -- 296
+-- );
+--
+-- delete from refdata_value where rdv_owner = (
+--     SELECT rdc_id FROM refdata_category WHERE rdc_description = 'Entitlement Issue Status'
+-- );
+--
+-- delete from refdata_category where rdc_id = (
+--     SELECT rdc_id FROM refdata_category WHERE rdc_description = 'Entitlement Issue Status'
+-- );
