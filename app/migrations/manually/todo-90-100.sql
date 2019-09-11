1a5ab5afbb6f358451b4e5bf17ac34c6f5c75eb7
anjaalbinnrw <albin@hbz-nrw.de>
2019-09-11 13:05:10 +0200

    bug-erms-1679-C

--- app/migrations/manually/todo-90-100.sql
+++ app/migrations/manually/todo-90-100.sql
@@ -143,3 +143,33 @@ update survey_result set surre_type_fk = (select surpro_id from survey_property 
 update survey_config set surconf_surprop_fk = (select surpro_id from survey_property where surpro_name = 'Participation') where surconf_surprop_fk = (select surpro_id from survey_property where surpro_name = 'Teilnahme');
 update survey_config_properties set surconpro_survey_property_fk = (select surpro_id from survey_property where surpro_name = 'Participation') where surconpro_survey_property_fk = (select surpro_id from survey_property where surpro_name = 'Teilnahme');
 
+-- START OF ERMS-1666 --
+-- Kontrolle vorher: zu löschende Einträge
+SELECT * FROM Person AS p
+                left join person_role pr on p.prs_id = pr_prs_fk
+WHERE p.prs_is_public = true
+  and pr isnull;
+
+
+DELETE FROM contact AS c
+WHERE c.ct_prs_fk in (SELECT p2.prs_id FROM Person AS p2
+                                           left join person_role pr on p2.prs_id = pr_prs_fk
+                   WHERE p2.prs_is_public = true
+                     and pr isnull);
+DELETE FROM address AS a
+WHERE a.adr_prs_fk in (SELECT p2.prs_id FROM Person AS p2
+                                           left join person_role pr on p2.prs_id = pr_prs_fk
+                   WHERE p2.prs_is_public = true
+                     and pr isnull);
+DELETE FROM Person AS p
+WHERE p.prs_id in (SELECT p2.prs_id FROM Person AS p2
+                                  left join person_role pr on p2.prs_id = pr_prs_fk
+               WHERE p2.prs_is_public = true
+                 and pr isnull);
+
+-- Kontrolle nachher: Liste müsste nun LEER sein!
+SELECT * FROM Person AS p
+                left join person_role pr on p.prs_id = pr_prs_fk
+WHERE p.prs_is_public = true
+  and pr isnull;
+-- END ERMS-1666 --
