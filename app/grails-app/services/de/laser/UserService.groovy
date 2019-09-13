e74fa4bd77f11255ce9cc007ea0f9481952d7516
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2019-09-13 11:49:06 +0200

    Hotfix Survey Notification

--- app/grails-app/services/de/laser/UserService.groovy
+++ app/grails-app/services/de/laser/UserService.groovy
@@ -1,7 +1,9 @@
 package de.laser
 
 import com.k_int.kbplus.auth.User
+import de.laser.helper.RDStore
 import org.codehaus.groovy.grails.commons.GrailsApplication
+import com.k_int.kbplus.UserSettings
 
 class UserService {
 
@@ -9,6 +11,10 @@ class UserService {
 
     void initMandatorySettings(User user) {
         log.debug('initMandatorySettings for user #' + user.id)
+
+
+        user.getSetting(UserSettings.KEYS.IS_NOTIFICATION_BY_EMAIL, RDStore.YN_YES)
+        user.getSetting(UserSettings.KEYS.IS_NOTIFICATION_FOR_SURVEYS_START, RDStore.YN_YES)
 
 // called after
 // new User.save()
