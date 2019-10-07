ec569d1fa4e8b79bbf5c23faedd7b18c66eb8085
David Klober <david.klober@hbz-nrw.de>
2019-10-07 13:37:46 +0200

    new dbm changelog

--- app/migrations/changelog.groovy
+++ app/migrations/changelog.groovy
@@ -7,36 +7,29 @@ databaseChangeLog = {
 	include file: 'changelog-0.groovy'		// migration to postgresql
 
 	include file: 'changelog-10.groovy'		// v0.12
-
 	include file: 'changelog-20.groovy'		// v0.13
-
 	// include file: 'changelog-20-qa.groovy' 	// QA only
 	// include file: 'changelog-20-prod.groovy'	// PROD only
 
 	include file: 'changelog-30.groovy'		// v0.14
-
 	include file: 'changelog-40.groovy'		// v0.15
-
 	include file: 'changelog-50.groovy'		// v0.16
-
 	// include file: 'changelog-50-qa.groovy' 	// QA only
 	// include file: 'changelog-50-prod.groovy'	// PROD only
 
 	include file: 'changelog-60.groovy'		// v0.17
 	include file: 'changelog-65.groovy'
-
 	include file: 'changelog-70.groovy'		// v0.18
 	include file: 'changelog-75.groovy'
 	include file: 'changelog-76.groovy'
-
 	include file: 'changelog-80.groovy'		// v0.19
-
 	include file: 'changelog-90.groovy'		// v0.20
 	include file: 'changelog-95.groovy'
 	include file: 'changelog-96.groovy'
 	include file: 'changelog-97.groovy'
 	include file: 'changelog-98.groovy'
-
 	include file: 'changelog-100.groovy'	// v1.0
 	include file: 'changelog-101.groovy'
+
+	include file: 'changelog-2019-10-07.groovy'	// new deployment strategy
 }
