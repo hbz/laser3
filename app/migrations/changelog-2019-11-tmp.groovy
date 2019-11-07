b47877aeacfdf412967c9f9c0425936a5dfc75e0
David Klober <david.klober@hbz-nrw.de>
2019-11-07 13:03:47 +0100

    added tmp: identifier db migration

--- app/migrations/changelog-2019-11-tmp.groovy
+++ app/migrations/changelog-2019-11-tmp.groovy
@@ -1,15 +1,18 @@
 databaseChangeLog = {
 
-	changeSet(author: "kloberd (modified)", id: "xx-1") {
+	changeSet(author: "kloberd (modified)", id: "tmp-1") {
 		grailsChange {
 			change {
 				sql.execute("create table identifier_distinct_tmp as select * from identifier with no data")
+				
+				sql.execute("create table identifier_backup as table identifier")
+				sql.execute("create table identifier_occurrence_backup as table identifier_occurrence")
 			}
 		}
 		rollback {}
 	}
 
-	changeSet(author: "kloberd (modified)", id: "xx-2") {
+	changeSet(author: "kloberd (modified)", id: "tmp-2") {
 		grailsChange {
 			change {
 				sql.execute("""
@@ -29,19 +32,11 @@ where i.id_value is not null and i.id_value != ''
 		}
 	}
 
-	changeSet(author: "kloberd (modified)", id: "xx-3") {
+	changeSet(author: "kloberd (modified)", id: "tmp-3") {
 		grailsChange {
 			change {
 				sql.execute("truncate table identifier RESTART IDENTITY cascade")
-			}
-			rollback {}
-		}
-	}
 
-	truncate table identifier RESTART IDENTITY cascade;
-	changeSet(author: "kloberd (modified)", id: "xx-4") {
-		grailsChange {
-			change {
 				sql.execute("""
 insert into identifier(
     version, id_value, id_ns_fk, id_ig_fk, id_lic_fk, id_org_fk, id_pkg_fk, id_sub_fk, id_ti_fk, id_tipp_fk, id_date_created, id_last_updated
@@ -55,14 +50,13 @@ from identifier_distinct_tmp
 		}
 	}
 
-	changeSet(author: "kloberd (modified)", id: "xx-5") {
+	changeSet(author: "kloberd (modified)", id: "tmp-4") {
 		grailsChange {
 			change {
 				sql.execute("drop table identifier_occurrence cascade")
+				sql.execute("drop table identifier_distinct_tmp")
 			}
 			rollback {}
 		}
 	}
-
-	// TODO DBM : delete identifier_distinct_tmp
 }
