d70bd44405f191a792fce7e6cb8fec7b92e7a0e5
anjaalbinnrw <albin@hbz-nrw.de>
2019-11-21 09:56:37 +0100

    Dashboard CronJob Zeit wieder auf 22:00 setzen

--- app/grails-app/jobs/de/laser/batch/DashboardDueDatesJob.groovy
+++ app/grails-app/jobs/de/laser/batch/DashboardDueDatesJob.groovy
@@ -10,10 +10,10 @@ class DashboardDueDatesJob extends AbstractJob {
     def grailsApplication
 
     static triggers = {
-//        cron name:'DashboardDueDatesTrigger', cronExpression: "0 0 22 * * ?" //Fire at 22:00 every day
+        cron name:'DashboardDueDatesTrigger', cronExpression: "0 0 22 * * ?" //Fire at 22:00 every day
 //        cron name:'DashboardDueDatesTrigger', cronExpression: "0 /15 * * * ?" //ONLY FOR DEVELOPMENT AND TESTS: Fire every 15th minute
 //        cron name:'DashboardDueDatesTrigger', cronExpression: "0 /5 * * * ?" //ONLY FOR DEVELOPMENT AND TESTS: Fire every 5th minute
-        cron name:'DashboardDueDatesTrigger', cronExpression: "0 /1 * * * ?" //ONLY FOR DEVELOPMENT AND TESTS: Fire every minute
+//        cron name:'DashboardDueDatesTrigger', cronExpression: "0 /1 * * * ?" //ONLY FOR DEVELOPMENT AND TESTS: Fire every minute
     }
 
     static configFlags = ['isUpdateDashboardTableInDatabase', 'isSendEmailsForDueDatesOfAllUsers']
