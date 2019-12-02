6a4f3d61552e7c6af2f48474ced76e3d1fa787c0
anjaalbinnrw <albin@hbz-nrw.de>
2019-12-02 10:42:50 +0100

    erms-1861

--- app/grails-app/controllers/com/k_int/kbplus/MyInstitutionController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/MyInstitutionController.groovy
@@ -2391,7 +2391,7 @@ AND EXISTS (
 
         DateFormat sdFormat    = new DateUtil().getSimpleDateFormat_NoTime()
         params.taskStatus = 'not done'
-        def query       = filterService.getTaskQuery(params, sdFormat)
+        def query       = filterService.getTaskQuery(params << [sort: 't.endDate', order: 'asc'], sdFormat)
         def contextOrg  = contextService.getOrg()
         result.tasks    = taskService.getTasksByResponsibles(springSecurityService.getCurrentUser(), contextOrg, query)
         result.tasksCount    = result.tasks.size()
@@ -3206,6 +3206,10 @@ AND EXISTS (
             }
         }
 
+        if ( ! params.sort) {
+            params.sort = "t.endDate"
+            params.order = "asc"
+        }
         DateFormat sdFormat = new DateUtil().getSimpleDateFormat_NoTime()
         def queryForFilter = filterService.getTaskQuery(params, sdFormat)
         int offset = params.offset ? Integer.parseInt(params.offset) : 0
