df0e3161adabb6a917cc86ce5637467a0ea7c9a7
anjaalbinnrw <albin@hbz-nrw.de>
2019-12-16 11:37:40 +0100

    ERMS-1985 Menu renew Sub is now also visible for administrative subs; renew Sub copies now also OrgRole subscriber_cons_hidden

--- app/grails-app/controllers/com/k_int/kbplus/SubscriptionController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/SubscriptionController.groovy
@@ -4365,7 +4365,7 @@ class SubscriptionController extends AbstractDebugController {
                 //OrgRole
                 baseSub.orgRelations?.each { or ->
 
-                    if ((or.org?.id == contextService.getOrg()?.id) || (or.roleType.value in ['Subscriber', 'Subscriber_Consortial'])) {
+                    if ((or.org?.id == contextService.getOrg()?.id) || (or.roleType in [RDStore.OR_SUBSCRIBER, RDStore.OR_SUBSCRIBER_CONS, RDStore.OR_SUBSCRIBER_CONS_HIDDEN])) {
                         OrgRole newOrgRole = new OrgRole()
                         InvokerHelper.setProperties(newOrgRole, or.properties)
                         newOrgRole.sub = newSub
