336e3a62c92324fbe872c8b24f13559e056081d3
David Klober <david.klober@hbz-nrw.de>
2020-01-27 12:01:34 +0100

    bugfix

--- app/grails-app/services/de/laser/StatsSyncService.groovy
+++ app/grails-app/services/de/laser/StatsSyncService.groovy
@@ -197,7 +197,7 @@ class StatsSyncService {
     }
 
     def getRelevantReportList(queryParams) {
-        List<RefdataValue> reports = RDConstants.TITLE_STATUS(RDConstants.FACT_TYPE)
+        List<RefdataValue> reports = RefdataCategory.getAllRefdataValues(RDConstants.FACT_TYPE)
         def availableReports = getAvailableReportsForPlatform(queryParams)
         reports.removeAll {
             if (it.value.startsWith('STATS') || (it.value.startsWith('JUSP'))){
