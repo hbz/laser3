7901781fc95f8af746605998b99c543a3d6325e7
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2020-03-09 17:03:53 +0100

    Bugfix dbm + Sort for Properties Views

--- app/migrations/changelog-2020-03-09.groovy
+++ app/migrations/changelog-2020-03-09.groovy
@@ -13,34 +13,34 @@ databaseChangeLog = {
 	changeSet(author: "djebeniani (modified)", id: "1583759022240-3") {
 		grailsChange {
 			change {
-				sql.execute("UPDATE subscription SET sub_kind_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.kind')\n" +
-						"                                                                            AND rdv_value = 'Alliance Licence')\n" +
-						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
+				sql.execute("UPDATE subscription SET sub_kind_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.kind') " +
+						"                                                                            AND rdv_value = 'Alliance Licence') " +
+						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
 						"AND rdv_value = 'Alliance Licence');")
 
-				sql.execute("UPDATE subscription SET sub_kind_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.kind')\n" +
-						"                                                                             AND rdv_value = 'National Licence')\n" +
-						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
+				sql.execute("UPDATE subscription SET sub_kind_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.kind') " +
+						"                                                                             AND rdv_value = 'National Licence') " +
+						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
 						"                                                           AND rdv_value = 'National Licence');")
 
-				sql.execute("UPDATE subscription SET sub_kind_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.kind')\n" +
-						"                                                                             AND rdv_value = 'Consortial Licence')\n" +
-						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
+				sql.execute("UPDATE subscription SET sub_kind_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.kind') " +
+						"                                                                             AND rdv_value = 'Consortial Licence') " +
+						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
 						"                                                           AND rdv_value = 'Consortial Licence');")
 
-				sql.execute("UPDATE subscription SET sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
-						"                                                                             AND rdv_value = 'Consortial Licence')\n" +
-						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
+				sql.execute("UPDATE subscription SET sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
+						"                                                                             AND rdv_value = 'Consortial Licence') " +
+						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
 						"                                                           AND rdv_value = 'National Licence');")
 
-				sql.execute("UPDATE subscription SET sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
-						"                                                                             AND rdv_value = 'Consortial Licence')\n" +
-						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type')\n" +
+				sql.execute("UPDATE subscription SET sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
+						"                                                                             AND rdv_value = 'Consortial Licence') " +
+						"WHERE sub_type_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.type') " +
 						"                                                           AND rdv_value = 'Alliance Licence');")
 			}
 			rollback {
@@ -63,16 +63,16 @@ databaseChangeLog = {
 		grailsChange {
 			change {
 
-				sql.execute("UPDATE subscription SET sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status')\n" +
-						"                                                                             AND rdv_value = 'Expired')\n" +
-						"WHERE sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status')\n" +
+				sql.execute("UPDATE subscription SET sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status') " +
+						"                                                                             AND rdv_value = 'Expired') " +
+						"WHERE sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status') " +
 						"                                                           AND rdv_value = 'ExpiredPerennial');")
 
-				sql.execute("UPDATE subscription SET sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE\n" +
-						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status')\n" +
-						"                                                                             AND rdv_value = 'Intended')\n" +
-						"WHERE sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status')\n" +
+				sql.execute("UPDATE subscription SET sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE " +
+						"        rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status') " +
+						"                                                                             AND rdv_value = 'Intended') " +
+						"WHERE sub_status_rv_fk = (SELECT rdv_id FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status') " +
 						"                                                           AND rdv_value = 'IntendedPerennial');")
 
 				sql.execute("DELETE FROM refdata_value WHERE rdv_owner = (SELECT rdc_id FROM refdata_category as rdc WHERE rdc.rdc_description = 'subscription.status') AND rdv_value = 'ExpiredPerennial';")
