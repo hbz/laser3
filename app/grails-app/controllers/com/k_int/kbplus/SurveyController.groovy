1cade62902f9dec6c5c7741037b67af6f93d209e
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2020-03-17 16:43:18 +0100

    Bugfix Survey

--- app/grails-app/controllers/com/k_int/kbplus/SurveyController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/SurveyController.groovy
@@ -4575,7 +4575,7 @@ class SurveyController {
         result.institution = contextService.getOrg()
         result.user = User.get(springSecurityService.principal.id)
         result.surveyInfo = SurveyInfo.get(params.id)
-        result.surveyConfig = SurveyConfig.get(params.surveyConfigID as Long? params.surveyConfigID: Long.parseLong(params.surveyConfigID)) ?: result.surveyInfo?.surveyConfigs[0]
+        result.surveyConfig = params.surveyConfigID ? SurveyConfig.get(params.surveyConfigID as Long ? params.surveyConfigID: Long.parseLong(params.surveyConfigID)) : result.surveyInfo?.surveyConfigs[0]
         result.surveyWithManyConfigs = (result.surveyInfo?.surveyConfigs?.size() > 1)
 
         result.editable = result.surveyInfo?.isEditable() ?: false
