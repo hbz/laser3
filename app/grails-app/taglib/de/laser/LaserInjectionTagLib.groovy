58c70b1f59852b792b5aea713f4765e1a76b565b
David Klober <david.klober@hbz-nrw.de>
2020-04-09 09:52:18 +0200

    added new var initializer for layout

--- app/grails-app/taglib/de/laser/LaserInjectionTagLib.groovy
+++ app/grails-app/taglib/de/laser/LaserInjectionTagLib.groovy
@@ -2,36 +2,23 @@ package de.laser
 
 class LaserInjectionTagLib {
 
-    def springSecurityService
-    def contextService
-    def accessService
-    def yodaService
-    def subscriptionsQueryService
-    def genericOIDService
-    def instAdmService
-    def surveyService
-    def taskService
-    def navigationGenerationService
-
     static namespace = "laser"
 
     def serviceInjection = { attrs, body ->
-        // HTML Comment not useful in Txt Mail
-        //out << "<!-- serviceInjection: springSecurityService, contextService, accessService, auditService, yodaService, subscriptionsQueryService, genericOIDService, instAdmService -->"
 
-        g.set( var:'springSecurityService', bean:'springSecurityService' )
-        g.set( var:'contextService', bean:'contextService' )
-        g.set( var:'cacheService', bean:'cacheService' )
         g.set( var:'accessService', bean:'accessService' )
         g.set( var:'auditService', bean:'auditService' )
-        g.set( var:'yodaService', bean:'yodaService' )
+        g.set( var:'cacheService', bean:'cacheService' )
+        g.set( var:'contextService', bean:'contextService' )
         g.set( var:'genericOIDService', bean:'genericOIDService' )
-        g.set( var:'subscriptionsQueryService', bean:'subscriptionsQueryService' )
         g.set( var:'instAdmService', bean:'instAdmService' )
-        g.set( var:'surveyService', bean:'surveyService' )
-        g.set( var:'orgDocumentService', bean:'orgDocumentService' )
-        g.set( var:'taskService', bean:'taskService' )
         g.set( var:'navigationGenerationService', bean:'navigationGenerationService' )
+        g.set( var:'orgDocumentService', bean:'orgDocumentService' )
         g.set( var:'pendingChangeService', bean:'pendingChangeService')
+        g.set( var:'springSecurityService', bean:'springSecurityService' )
+        g.set( var:'subscriptionsQueryService', bean:'subscriptionsQueryService' )
+        g.set( var:'surveyService', bean:'surveyService' )
+        g.set( var:'taskService', bean:'taskService' )
+        g.set( var:'yodaService', bean:'yodaService' )
     }
 }
