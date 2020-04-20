2e8bea9096c86776cd6c6bfbbcae94ab4be59afa
David Klober <david.klober@hbz-nrw.de>
2020-04-20 11:04:04 +0200

    added dbm functionality

--- app/grails-app/controllers/de/laser/MigrationsController.groovy
+++ app/grails-app/controllers/de/laser/MigrationsController.groovy
@@ -19,7 +19,7 @@ class MigrationsController {
 
     @Secured(['ROLE_YODA'])
     def erms2362() {
-        // List<String> nsList = ['acs', 'ebookcentral', 'emerald', 'herdt', 'mitpress', 'oup']
+        List<String> validNsList = ['acs', 'ebookcentral', 'emerald', 'herdt', 'mitpress', 'oup']
 
         Closure pre = { tmp ->
             "<pre>" + tmp + "</pre>"
@@ -27,21 +27,25 @@ class MigrationsController {
         Closure code = { tmp ->
             '<code>' + tmp.join('<br>') + '</code>'
         }
-        Closure href = { reference ->
-            String link = reference instanceof TitleInstance ? '/title/show/' : (
-                          reference instanceof License ? '/lic/show/' : (
-                          reference instanceof Subscription ? '/subscription/show/' : (
-                          reference instanceof Package ? '/package/show/' : (
-                          reference instanceof Org ? '/org/show/' : (
-                          null
-            )))))
-
-            return link ? (grailsApplication.config.grails.serverURL + link + reference.id) : null
-        }
 
         String result       = ''
         String ns_str       = params.ns ?: null
         String ns_old_str   = ns_str ? 'inid_' + ns_str : null
+
+        result += '<ul>'
+        validNsList.each { vns ->
+            result += '<li><a href="' + grailsApplication.config.grails.serverURL + '/migrations/erms2362?ns=' + vns + '">' + vns + '</a></li>'
+        }
+        result += '</ul>'
+        result += '<hl/>'
+
+        if (! validNsList.contains(ns_str)) {
+            render text: '<!DOCTYPE html><html lang="en"><head><title>' +
+                    grailsApplication.config.grails.serverURL +
+                    '</title></head><body><pre>Namespace <strong>' + ns_str + '</strong> is not positive listed .. process canceled</pre></body></html>'
+
+            return
+        }
 
         long ts = System.currentTimeMillis()
 
@@ -51,67 +55,36 @@ class MigrationsController {
         IdentifierNamespace newNs = IdentifierNamespace.findByNs(ns_str)
 
         if (oldNs && newNs) {
-            List<Identifier> oldIds = Identifier.findAllByNs(oldNs)
-            List matches  = []
-            List orphaned = []
+            Map<String, Object> crs = calcErms2362(oldNs, newNs)
 
-            result += "<h2>NS:${oldNs.id} <strong>${oldNs.ns}</strong> <-- NS:${newNs.id} <strong>${newNs.ns}</strong></h2>"
-            result += pre("${oldIds.size()} identifier/s in old namespace found")
+            List matches  = crs.matches
+            List orphaned = crs.orphaned
 
-            oldIds.eachWithIndex { old, i ->
-                log.debug('processing (' + (i+1) + ' from ' + oldIds.size() + '): ' + old)
+            // frontend
 
-                List<Identifier> newIds = Identifier.findAllWhere([
-                        value: old.value,
-                        ns:   newNs,
-                        lic:  old.lic,
-                        org:  old.org,
-                        pkg:  old.pkg,
-                        sub:  old.sub,
-                        ti:   old.ti,
-                        tipp: old.tipp,
-                        cre:  old.cre
-                ])
-                def reference = old.getReference()
-
-                if (! newIds.isEmpty()) {
-                    matches.add([
-                            oldNs: oldNs,
-                            oldId: old,
-                            newNs: newNs,
-                            newIds: newIds,
-                            link: href(reference)
-                    ])
-                }
-                else {
-                    orphaned.add([
-                            oldNs: oldNs,
-                            oldId: old,
-                            reference: [
-                                    id: reference.id,
-                                    gokbId: reference.hasProperty('gokbId') ? reference.gokbId : null,
-                                    type: reference.getClass()?.canonicalName,
-                                    link: href(reference)
-                            ]
-                    ])
-                }
-            }
+            result += "<h2>NS:${oldNs.id} <strong>${oldNs.ns}</strong> << NS:${newNs.id} <strong>${newNs.ns}</strong></h2>"
+            result += pre("${matches.size() + orphaned.size()} identifier/s in old namespace found")
             result += pre("${matches.size()} matched, ${orphaned.size()} orphaned")
+
+            if (params.cmd == 'execute') {
+                result += '<p style="color:red">MIGRATION EXECUTED, RELOAD PAGE TO REFRESH</p>'
+            }
 
             if (! matches.isEmpty()) {
                 String[] m = []
                 matches.each { c ->
-                    String tmp = "[ NS:${c.oldNs.id} <strong>${c.oldNs.ns}</strong> / ID:${c.oldId.id} <strong>${c.oldId.value}</strong> ]"
-                    tmp += " <strong><--</strong> " + (c.newIds.collect{ it ->
+                    String tmp = "([ NS:${c.oldNs.id} <strong>${c.oldNs.ns}</strong> / ID:${c.oldId.id} <strong>${c.oldId.value}</strong> ]"
+                    tmp += " <strong><<</strong> " + (c.newIds.collect{ it ->
                         "[ NS:${c.newNs.id} <strong>${c.newNs.ns}</strong> / ID:${it.id} <strong>${it.value}</strong> ]"
                     }).join(", ")
+                    tmp += ")"
 
                     if( c.link) {
-                        tmp += " -----> [ <a href='${c.link}' target='_blank'>${c.link.replace(grailsApplication.config.grails.serverURL,'')}</a> ]"
+                        tmp += " =-----> [ <a href='${c.link}' target='_blank'>${c.link.replace(grailsApplication.config.grails.serverURL,'')}</a> ]"
                     }
                     m += tmp
                 }
-                result += pre('Matches:')
+                result += pre('--- Matches:')
                 result += code(m)
             }
 
@@ -119,7 +92,7 @@ class MigrationsController {
                 String[] o = []
                 orphaned.each { c ->
                     String tmp = "[ NS:${c.oldNs.id} <strong>${c.oldNs.ns}</strong> / ID:${c.oldId.id} <strong>${c.oldId.value}</strong> ]"
-                    tmp += " -----> [ ${c.reference.type}:${c.reference.id} - "
+                    tmp += " =-----> [ ${c.reference.type}:${c.reference.id} - "
 
                     if (c.reference.gokbId) {
                         tmp += "GOKBID:${c.reference.gokbId} - "
@@ -129,10 +102,30 @@ class MigrationsController {
                     }
                     o += tmp
                 }
-                result += pre('Orphans:')
+                result += pre('--- Orphans:')
                 result += code(o)
             }
             result += '<br/><br/>'
+
+            log.debug( 'matches: ' + matches )
+            log.debug( 'orphaned: ' + orphaned )
+
+            // processing
+
+            if (params.cmd == 'execute') {
+                log.debug( 'execute ..')
+                if (! matches.isEmpty()) {
+                    matches.each { c ->
+                        ((Identifier) c.oldId).delete()
+                    }
+                }
+                if (! orphaned.isEmpty()) {
+                    orphaned.each { c ->
+                        ((Identifier) c.oldId).setNs(c.newNs)
+                        ((Identifier) c.oldId).save()
+                    }
+                }
+            }
         }
         else {
             result += oldNs ? '' : pre('<strong>ERROR</strong>: old namespace <strong>' + ns_old_str + '</strong> not found')
@@ -146,6 +139,66 @@ class MigrationsController {
         ])
 
         render text: '<!DOCTYPE html><html lang="en"><head><title>' + grailsApplication.config.grails.serverURL + '</title></head><body>' + result + '</body></html>'
+    }
+
+    private Map<String, Object> calcErms2362(IdentifierNamespace oldNs, IdentifierNamespace newNs) {
+
+        Closure href = { reference ->
+            String link = reference instanceof TitleInstance ? '/title/show/' : (
+                    reference instanceof License ? '/lic/show/' : (
+                            reference instanceof Subscription ? '/subscription/show/' : (
+                                    reference instanceof Package ? '/package/show/' : (
+                                            reference instanceof Org ? '/org/show/' : (
+                                                    null
+                                            )))))
+
+            return link ? (grailsApplication.config.grails.serverURL + link + reference.id) : null
+        }
+
+        List<Identifier> oldIds = Identifier.findAllByNs(oldNs)
+        List matches = []
+        List orphaned = []
+
+        oldIds.eachWithIndex { old, i ->
+            log.debug('processing (' + (i + 1) + ' from ' + oldIds.size() + '): ' + old)
+
+            List<Identifier> newIds = Identifier.findAllWhere([
+                    value: old.value,
+                    ns   : newNs,
+                    lic  : old.lic,
+                    org  : old.org,
+                    pkg  : old.pkg,
+                    sub  : old.sub,
+                    ti   : old.ti,
+                    tipp : old.tipp,
+                    cre  : old.cre
+            ])
+            def reference = old.getReference()
+
+            if (!newIds.isEmpty()) {
+                matches.add([
+                        oldNs : oldNs,
+                        oldId : old,
+                        newNs : newNs,
+                        newIds: newIds,
+                        link  : href(reference)
+                ])
+            } else {
+                orphaned.add([
+                        oldNs    : oldNs,
+                        oldId    : old,
+                        newNs    : newNs,
+                        reference: [
+                                id    : reference.id,
+                                gokbId: reference.hasProperty('gokbId') ? reference.gokbId : null,
+                                type  : reference.getClass()?.canonicalName,
+                                link  : href(reference)
+                        ]
+                ])
+            }
+        }
+
+        return [ matches: matches, orphaned: orphaned ]
     }
 
 }
