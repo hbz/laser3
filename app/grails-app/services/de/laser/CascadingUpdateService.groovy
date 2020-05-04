7cb6a7ac2c03816071847e301e141a5ff89292e7
David Klober <david.klober@hbz-nrw.de>
2020-05-04 10:56:34 +0200

    ..

--- app/grails-app/services/de/laser/CascadingUpdateService.groovy
+++ app/grails-app/services/de/laser/CascadingUpdateService.groovy
@@ -10,24 +10,24 @@ class CascadingUpdateService {
 
     def grailsApplication
 
-    def update(IdentifierNamespace obj, Date clu) {
-        toLog(obj, clu)
+    def update(IdentifierNamespace obj, Date luc) {
+        toLog(obj, luc)
 
-        IdentifierNamespace.executeUpdate("update IdentifierNamespace ns set ns.lastUpdatedCascading = :clu where ns = :obj", [
-                clu: clu, obj: obj
+        IdentifierNamespace.executeUpdate("update IdentifierNamespace ns set ns.lastUpdatedCascading = :luc where ns = :obj", [
+                luc: luc, obj: obj
         ])
 
-        Identifier.findAllByNs(obj).each{ i -> update(i, clu) }
+        Identifier.findAllByNs(obj).each{ i -> update(i, luc) }
     }
 
-    def update(Identifier obj, Date clu) {
-        toLog(obj, clu)
+    def update(Identifier obj, Date luc) {
+        toLog(obj, luc)
 
-        Identifier.executeUpdate("update Identifier i set i.lastUpdatedCascading = :clu where i = :obj", [
-                clu: clu, obj: obj
+        Identifier.executeUpdate("update Identifier i set i.lastUpdatedCascading = :luc where i = :obj", [
+                luc: luc, obj: obj
         ])
 
-        if (obj.sub) { update(obj.sub, clu) }
+        if (obj.sub) { update(obj.sub, luc) }
 
 //        lic:    License,
 //        org:    Org,
@@ -37,15 +37,15 @@ class CascadingUpdateService {
 //        cre:    Creator
     }
 
-    def update(Subscription obj, Date clu) {
-        toLog(obj, clu)
+    def update(Subscription obj, Date luc) {
+        toLog(obj, luc)
 
-        Subscription.executeUpdate("update Subscription sub set sub.lastUpdatedCascading = :clu where sub = :obj", [
-                clu: clu, obj: obj
+        Subscription.executeUpdate("update Subscription sub set sub.lastUpdatedCascading = :luc where sub = :obj", [
+                luc: luc, obj: obj
         ])
     }
 
-    private void toLog(Object obj, Date clu) {
-        log.debug ('cascadingUpdate for ' + obj + ' = ' + obj.cascadingLastUpdated + ' <- ' + clu)
+    private void toLog(Object obj, Date luc) {
+        log.debug ('cascadingUpdate for ' + obj + ' = [' + obj.cascadingLastUpdated + '] <- [' + luc + ']')
     }
 }
