4bd9ec1237d46c591887e166e9cb50367970eaa8
Moe <moetez.djebeniani@hbz-nrw.de>
2020-06-03 13:27:29 +0200

    Remakrs

--- app/grails-app/services/de/laser/SubscriptionService.groovy
+++ app/grails-app/services/de/laser/SubscriptionService.groovy
@@ -1707,7 +1707,7 @@ class SubscriptionService {
                     ((colMap.printIdentifierCol >= 0 && cols[colMap.printIdentifierCol].trim().isEmpty()) || colMap.printIdentifierCol < 0)) {
             } else {
 
-                List<TitleInstance> titleIds = TitleInstance.executeQuery('select ti.id from TitleInstance ti join ti.ids ident where ident.ns in :namespaces and ident.value = :value', [namespaces:idCandidate.namespaces, value:idCandidate.value])
+                List<Long> titleIds = TitleInstance.executeQuery('select ti.id from TitleInstance ti join ti.ids ident where ident.ns in :namespaces and ident.value = :value', [namespaces:idCandidate.namespaces, value:idCandidate.value])
                 if (titleIds.size() > 0) {
                     IssueEntitlement issueEntitlement = issueEntitlements.find { it.tipp.title.id in titleIds }
                     IssueEntitlementCoverage ieCoverage = new IssueEntitlementCoverage()
