e2852a05ddf4a5f3f253298188436067321854e3
Moe <moetez.djebeniani@hbz-nrw.de>
2020-06-03 11:19:38 +0200

    Bugfix issueEntitlementEnrichment

--- app/grails-app/services/de/laser/SubscriptionService.groovy
+++ app/grails-app/services/de/laser/SubscriptionService.groovy
@@ -1707,9 +1707,9 @@ class SubscriptionService {
                     ((colMap.printIdentifierCol >= 0 && cols[colMap.printIdentifierCol].trim().isEmpty()) || colMap.printIdentifierCol < 0)) {
             } else {
 
-                TitleInstance tiObj = TitleInstance.executeQuery('select ti from TitleInstance ti join ti.ids ident where ident.ns in :namespaces and ident.value = :value', [namespaces:idCandidate.namespaces, value:idCandidate.value])
-                if (tiObj) {
-                    IssueEntitlement issueEntitlement = issueEntitlements.find { it.tipp.title.id == tiObj.id }
+                List<TitleInstance> tiObj = TitleInstance.executeQuery('select ti from TitleInstance ti join ti.ids ident where ident.ns in :namespaces and ident.value = :value', [namespaces:idCandidate.namespaces, value:idCandidate.value])
+                if (tiObj.size() > 0) {
+                    IssueEntitlement issueEntitlement = issueEntitlements.find { it.tipp.title.id in tiObj.id }
                     IssueEntitlementCoverage ieCoverage = new IssueEntitlementCoverage()
                     if (issueEntitlement) {
                         count++
