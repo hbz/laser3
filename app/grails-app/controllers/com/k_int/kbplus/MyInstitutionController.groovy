fc816dfb194852f725d5f6112acfbdb641a07f62
agalffy <andrisgalffy@gmail.com>
2020-08-18 09:50:39 +0200

    section commit for August 18th, '20
    the pie chart for the provider entry is not really sutiable, check alternatives!

--- app/grails-app/controllers/com/k_int/kbplus/MyInstitutionController.groovy
+++ app/grails-app/controllers/com/k_int/kbplus/MyInstitutionController.groovy
@@ -137,7 +137,7 @@ class MyInstitutionController extends AbstractDebugController {
 
     @DebugAnnotation(test='hasAffiliation("INST_USER")')
     @Secured(closure = { ctx.springSecurityService.getCurrentUser()?.hasAffiliation("INST_USER") })
-    def loadCostItemChartData() {
+    def loadChartData() {
         Org contextOrg = contextService.org
         Map<String,Object> baseMap = financeService.getCostItemsFromEntryPoint([subscription:params.subscription,package:params.package,institution:contextOrg]), result = [:]
         if(baseMap.costItems)
@@ -145,7 +145,9 @@ class MyInstitutionController extends AbstractDebugController {
         if(contextOrg.getCustomerType() == 'ORG_CONSORTIUM' && params.subscription)
             result.graphB = subscriptionService.getSubscribersByRegion([subscription:params.subscription,institution:contextOrg])
         else if(params.subscriber)
-            result.graphC = organisationService.getSubscriberProviderPercentages([subscriber:params.subscriber,institution:contextOrg])
+            result.graphC = organisationService.getSubscriberProviderPercentages([subscriber: params.subscriber, institution: contextOrg])
+        else if(params.provider)
+            result.graphB = organisationService.getSubscriberProviderPercentages([provider: params.provider, institution: contextOrg])
         render result as JSON
     }
 
