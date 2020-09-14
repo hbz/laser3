0180af8a2f12d233bd262ce39b677fa0c346eda8
David Klober <david.klober@hbz-nrw.de>
2020-09-14 14:25:20 +0200

    removed OrgTitleStats and SystemObject

--- app/grails-app/services/de/laser/BootStrapService.groovy
+++ app/grails-app/services/de/laser/BootStrapService.groovy
@@ -9,7 +9,6 @@ import com.k_int.kbplus.RefdataCategory
 import com.k_int.kbplus.RefdataValue
 import com.k_int.kbplus.Setting
 import com.k_int.kbplus.Subscription
-import com.k_int.kbplus.SystemObject
 import com.k_int.kbplus.auth.*
 import de.laser.helper.*
 import de.laser.properties.PropertyDefinition
@@ -46,10 +45,6 @@ class BootStrapService {
         log.info("Documents: " + ConfigUtils.getDocumentStorageLocation())
 
         log.info("--------------------------------------------------------------------------------")
-
-        if (ConfigUtils.getLaserSystemId() != null) {
-            SystemObject system_object = SystemObject.findBySysId(ConfigUtils.getLaserSystemId().toString()) ?: new SystemObject(sysId: ConfigUtils.getLaserSystemId()).save(flush: true)
-        }
 
         SystemEvent.createEvent('BOOTSTRAP_STARTUP')
 
--- app/grails-app/services/de/laser/DeletionService.groovy
+++ app/grails-app/services/de/laser/DeletionService.groovy
@@ -511,7 +511,6 @@ class DeletionService {
         List facts              = Fact.findAllByInst(org)
         List readerNumbers      = ReaderNumber.findAllByOrg(org)
         List orgAccessPoints    = OrgAccessPoint.findAllByOrg(org)
-        List orgTitleStats      = OrgTitleStats.findAllByOrg(org)
 
         List surveyInfos        = SurveyInfo.findAllByOwner(org)
         List surveyProperties   = PropertyDefinition.getAllByDescrAndTenant(PropertyDefinition.SUR_PROP, org)
@@ -566,7 +565,6 @@ class DeletionService {
         result.info << ['Facts', facts, FLAG_BLOCKER]
         result.info << ['ReaderNumbers', readerNumbers, FLAG_BLOCKER]
         result.info << ['OrgAccessPoints', orgAccessPoints, FLAG_BLOCKER]
-        result.info << ['OrgTitleStats', orgTitleStats, FLAG_BLOCKER]
 
         result.info << ['SurveyInfos', surveyInfos, FLAG_BLOCKER]
         result.info << ['Umfrage-Merkmale', surveyProperties, FLAG_BLOCKER]
