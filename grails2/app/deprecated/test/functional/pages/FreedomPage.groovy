5024622fef3bca69150b6d0ab5a6d263237d016c
David Klober <david.klober@hbz-nrw.de>
2020-10-19 12:14:20 +0200

    removed deprecated legacy

--- grails2/app/deprecated/test/functional/pages/FreedomPage.groovy
+++
@@ -1,8 +0,0 @@
-package pages
-/**
- * Created by ioannis on 28/05/2014.
- */
-class FreedomPage extends BasePage {
-    static url = "/laser/freedom-of-information-policy"
-    static at = { browser.page.title.startsWith "Freedom of Information" }
-}
--- grails2/app/deprecated/test/functional/pages/PrivacyPage.groovy
+++
@@ -1,9 +0,0 @@
-package pages
-/**
- * Created by ioannis on 28/05/2014.
- */
-class PrivacyPage extends BasePage {
-	static url = "/laser/privacy-policy"
-    static at = { browser.page.title.startsWith "Privacy Policy" }
-
-}
