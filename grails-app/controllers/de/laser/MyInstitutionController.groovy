d5d9bc3aa67d1d8ccfafb70b15320c7651977a90
David Klober <david.klober@hbz-nrw.de>
2020-10-27 11:13:24 +0100

    draft: controller services

--- grails-app/controllers/de/laser/MyInstitutionController.groovy
+++ grails-app/controllers/de/laser/MyInstitutionController.groovy
@@ -3,9 +3,9 @@ package de.laser
 
 import com.k_int.kbplus.ExportService
 import com.k_int.kbplus.InstitutionsService
+import de.laser.ctrl.MyInstitutionControllerService
 import de.laser.properties.LicenseProperty
 import de.laser.properties.OrgProperty
-import com.k_int.kbplus.PackageService
 import com.k_int.kbplus.PendingChangeService
 import de.laser.properties.PersonProperty
 import de.laser.properties.PlatformProperty
@@ -54,13 +54,15 @@ import java.text.SimpleDateFormat
 
 @Secured(['IS_AUTHENTICATED_FULLY'])
 class MyInstitutionController  {
+
+    def myInstitutionControllerService
+
     def dataSource
     def springSecurityService
     def userService
     def genericOIDService
     PendingChangeService pendingChangeService
     ExportService exportService
-    PackageService packageService
     def escapeService
     def institutionsService
     def docstoreService
@@ -77,7 +79,6 @@ class MyInstitutionController  {
     def financeService
     def surveyService
     def formService
-    def dashboardDueDatesService
     LinksGenerationService linksGenerationService
     ComparisonService comparisonService
 
@@ -1589,77 +1590,18 @@ join sub.orgRelations or_sub where
     @DebugAnnotation(test = 'hasAffiliation("INST_USER")')
     @Secured(closure = { principal.user?.hasAffiliation("INST_USER") })
     def dashboard() {
-        Map<String, Object> result = setResultGenerics()
 
-        if (! accessService.checkUserIsMember(result.user, result.institution)) {
-            flash.error = "You do not have permission to access ${result.institution.name} pages. Please request access on the profile page";
+        Map<String, Object> ctrlResult = myInstitutionControllerService.dashboard(this, params)
+
+        if (ctrlResult.status == MyInstitutionControllerService.STATUS_OK) {
+            return ctrlResult.result
+        }
+        else {
+            flash.error = "You do not have permission to access ${result.institution.name} pages. Please request access on the profile page"
             response.sendError(401)
-            return;
         }
-
-        result.is_inst_admin = accessService.checkMinUserOrgRole(result.user, result.institution, 'INST_ADM')
-        result.max = params.max ? Integer.parseInt(params.max) : result.user.getDefaultPageSizeAsInteger()
-        result.offset = params.offset ? Integer.parseInt(params.offset) : 0
-        result.pendingOffset = 0
-        result.acceptedOffset = 0
-        result.dashboardDueDatesOffset = 0
-        switch(params.view) {
-            case 'PendingChanges': result.pendingOffset = result.offset
-            break
-            case 'AcceptedChanges': result.acceptedOffset = result.offset
-            break
-            case 'dueDatesView': result.dashboardDueDatesOffset = result.offset
-            break
-        }
-
-        def periodInDays = contextService.getUser().getSettingsValue(UserSetting.KEYS.DASHBOARD_ITEMS_TIME_WINDOW, 14)
-
-        // changes
-
-        Map<String,Object> pendingChangeConfigMap = [contextOrg:result.institution,consortialView:accessService.checkPerm(result.institution,"ORG_CONSORTIUM"),periodInDays:periodInDays,max:result.max,pendingOffset:result.pendingOffset,acceptedOffset:result.acceptedOffset,pending:true,notifications:true]
-
-        result.putAll(pendingChangeService.getChanges(pendingChangeConfigMap))
-
-        // systemAnnouncements
-
-        result.systemAnnouncements = SystemAnnouncement.getPublished(periodInDays)
-
-        // tasks
-
-        SimpleDateFormat sdFormat    = DateUtil.getSDF_NoTime()
-        params.taskStatus = 'not done'
-        def query       = filterService.getTaskQuery(params << [sort: 't.endDate', order: 'asc'], sdFormat)
-        Org contextOrg  = contextService.getOrg()
-        result.tasks    = taskService.getTasksByResponsibles(springSecurityService.getCurrentUser(), contextOrg, query)
-        result.tasksCount    = result.tasks.size()
-        result.enableMyInstFormFields = true // enable special form fields
-
-
-        /*def announcement_type = RefdataValue.getByValueAndCategory('Announcement', RDConstants.DOCUMENT_TYPE)
-        result.recentAnnouncements = Doc.findAllByType(announcement_type, [max: result.max,offset:result.announcementOffset, sort: 'dateCreated', order: 'desc'])
-        result.recentAnnouncementsCount = Doc.findAllByType(announcement_type).size()*/
-
-        result.dueDates = dashboardDueDatesService.getDashboardDueDates( contextService.user, contextService.org, false, false, result.max, result.dashboardDueDatesOffset)
-        result.dueDatesCount = dashboardDueDatesService.getDashboardDueDates(contextService.user, contextService.org, false, false).size()
-
-        List activeSurveyConfigs = SurveyConfig.executeQuery("from SurveyConfig surConfig where exists (select surOrg from SurveyOrg surOrg where surOrg.surveyConfig = surConfig AND surOrg.org = :org and surOrg.finishDate is null and surConfig.pickAndChoose = true and surConfig.surveyInfo.status = :status) " +
-                " or exists (select surResult from SurveyResult surResult where surResult.surveyConfig = surConfig and surConfig.surveyInfo.status = :status and surResult.finishDate is null and surResult.participant = :org) " +
-                " order by surConfig.surveyInfo.endDate",
-                [org: result.institution,
-                 status: RDStore.SURVEY_SURVEY_STARTED])
-
-        if(accessService.checkPerm('ORG_CONSORTIUM')){
-            activeSurveyConfigs = SurveyConfig.executeQuery("from SurveyConfig surConfig where surConfig.surveyInfo.status = :status  and surConfig.surveyInfo.owner = :org " +
-                    " order by surConfig.surveyInfo.endDate",
-                    [org: result.institution,
-                     status: RDStore.SURVEY_SURVEY_STARTED])
-        }
-
-        result.surveys = activeSurveyConfigs.groupBy {it?.id}
-        result.countSurvey = result.surveys.size()
-
-        result
     }
+
     @DebugAnnotation(test = 'hasAffiliation("INST_USER")')
     @Secured(closure = { principal.user?.hasAffiliation("INST_USER") })
     def modal_create() {
---
+++ grails-app/services/de/laser/ctrl/MyInstitutionControllerService.groovy
@@ -0,0 +1,101 @@
+package de.laser.ctrl
+
+import de.laser.MyInstitutionController
+import de.laser.Org
+import de.laser.SurveyConfig
+import de.laser.UserSetting
+import de.laser.helper.DateUtil
+import de.laser.helper.RDStore
+import de.laser.system.SystemAnnouncement
+import grails.gorm.transactions.Transactional
+import grails.web.servlet.mvc.GrailsParameterMap
+
+import java.text.SimpleDateFormat
+
+@Transactional
+class MyInstitutionControllerService {
+
+    def accessService
+    def contextService
+    def dashboardDueDatesService
+    def filterService
+    def pendingChangeService
+    def springSecurityService
+    def taskService
+
+    static final int STATUS_OK = 1
+    static final int STATUS_ERROR = 2
+
+    Map<String, Object> dashboard (MyInstitutionController controller, GrailsParameterMap params) {
+
+        Map<String, Object> result = controller.setResultGenerics()
+
+        if (! accessService.checkUserIsMember(result.user, result.institution)) {
+            return [status: STATUS_ERROR, result: result]
+        }
+
+        result.is_inst_admin = accessService.checkMinUserOrgRole(result.user, result.institution, 'INST_ADM')
+        result.max = params.max ? Integer.parseInt(params.max) : result.user.getDefaultPageSizeAsInteger()
+        result.offset = params.offset ? Integer.parseInt(params.offset) : 0
+        result.pendingOffset = 0
+        result.acceptedOffset = 0
+        result.dashboardDueDatesOffset = 0
+        switch(params.view) {
+            case 'PendingChanges': result.pendingOffset = result.offset
+                break
+            case 'AcceptedChanges': result.acceptedOffset = result.offset
+                break
+            case 'dueDatesView': result.dashboardDueDatesOffset = result.offset
+                break
+        }
+
+        def periodInDays = contextService.getUser().getSettingsValue(UserSetting.KEYS.DASHBOARD_ITEMS_TIME_WINDOW, 14)
+
+        // changes
+
+        Map<String,Object> pendingChangeConfigMap = [contextOrg:result.institution,consortialView:accessService.checkPerm(result.institution,"ORG_CONSORTIUM"),periodInDays:periodInDays,max:result.max,pendingOffset:result.pendingOffset,acceptedOffset:result.acceptedOffset,pending:true,notifications:true]
+
+        result.putAll(pendingChangeService.getChanges(pendingChangeConfigMap))
+
+        // systemAnnouncements
+
+        result.systemAnnouncements = SystemAnnouncement.getPublished(periodInDays)
+
+        // tasks
+
+        SimpleDateFormat sdFormat    = DateUtil.getSDF_NoTime()
+        params.taskStatus = 'not done'
+        def query       = filterService.getTaskQuery(params << [sort: 't.endDate', order: 'asc'], sdFormat)
+        Org contextOrg  = contextService.getOrg()
+        result.tasks    = taskService.getTasksByResponsibles(springSecurityService.getCurrentUser(), contextOrg, query)
+        result.tasksCount    = result.tasks.size()
+        result.enableMyInstFormFields = true // enable special form fields
+
+
+        /*def announcement_type = RefdataValue.getByValueAndCategory('Announcement', RDConstants.DOCUMENT_TYPE)
+        result.recentAnnouncements = Doc.findAllByType(announcement_type, [max: result.max,offset:result.announcementOffset, sort: 'dateCreated', order: 'desc'])
+        result.recentAnnouncementsCount = Doc.findAllByType(announcement_type).size()*/
+
+        result.dueDates = dashboardDueDatesService.getDashboardDueDates( contextService.user, contextService.org, false, false, result.max, result.dashboardDueDatesOffset)
+        result.dueDatesCount = dashboardDueDatesService.getDashboardDueDates(contextService.user, contextService.org, false, false).size()
+
+        List activeSurveyConfigs = SurveyConfig.executeQuery("from SurveyConfig surConfig where exists (select surOrg from SurveyOrg surOrg where surOrg.surveyConfig = surConfig AND surOrg.org = :org and surOrg.finishDate is null and surConfig.pickAndChoose = true and surConfig.surveyInfo.status = :status) " +
+                " or exists (select surResult from SurveyResult surResult where surResult.surveyConfig = surConfig and surConfig.surveyInfo.status = :status and surResult.finishDate is null and surResult.participant = :org) " +
+                " order by surConfig.surveyInfo.endDate",
+                [org: result.institution,
+                 status: RDStore.SURVEY_SURVEY_STARTED])
+
+        if(accessService.checkPerm('ORG_CONSORTIUM')){
+            activeSurveyConfigs = SurveyConfig.executeQuery("from SurveyConfig surConfig where surConfig.surveyInfo.status = :status  and surConfig.surveyInfo.owner = :org " +
+                    " order by surConfig.surveyInfo.endDate",
+                    [org: result.institution,
+                     status: RDStore.SURVEY_SURVEY_STARTED])
+        }
+
+        result.surveys = activeSurveyConfigs.groupBy {it?.id}
+        result.countSurvey = result.surveys.size()
+
+
+        [status: STATUS_OK, result: result]
+    }
+}
