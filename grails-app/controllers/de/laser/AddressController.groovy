4c6e105d80d7dab0ed4c223dc8a2166630ee05f2
David Klober <david.klober@hbz-nrw.de>
2020-10-29 13:40:13 +0100

    address, contact and person

--- grails-app/controllers/de/laser/AddressController.groovy
+++ grails-app/controllers/de/laser/AddressController.groovy
@@ -1,9 +1,8 @@
 package de.laser
-
-
  
 import de.laser.helper.DebugAnnotation
 import de.laser.helper.RDStore
+import grails.gorm.transactions.Transactional
 import grails.plugin.springsecurity.annotation.Secured
 import org.springframework.dao.DataIntegrityViolationException
 
@@ -12,7 +11,6 @@ class AddressController  {
 
 	def springSecurityService
 	def addressbookService
-    def contextService
     def formService
 
     static allowedMethods = [create: ['GET', 'POST'], edit: ['GET', 'POST'], delete: 'POST']
@@ -25,38 +23,39 @@ class AddressController  {
     @DebugAnnotation(test='hasAffiliation("INST_EDITOR")')
     @Secured(closure = { principal.user?.hasAffiliation("INST_EDITOR") })
     def create() {
-		switch (request.method) {
-			case 'GET':
-				[addressInstance: new Address(params)]
-				break
-			case 'POST':
-                if (formService.validateToken(params)) {
-                    Address addressInstance = new Address()
+        Address.withTransaction {
+            switch (request.method) {
+                case 'GET':
+                    [addressInstance: new Address(params)]
+                    break
+                case 'POST':
+                    if (formService.validateToken(params)) {
+                        Address addressInstance = new Address()
 
-                    params.list('type.id').each {
-                        if(!(it in addressInstance.type))
-                        {
-                            addressInstance.addToType(RefdataValue.get(Long.parseLong(it)))
+                        params.list('type.id').each {
+                            if (!(it in addressInstance.type)) {
+                                addressInstance.addToType(RefdataValue.get(Long.parseLong(it)))
+                            }
                         }
+
+                        params.remove('type.id')
+
+                        addressInstance.properties = params
+
+                        if (!addressInstance.save()) {
+                            flash.error = message(code: 'default.save.error.general.message')
+                            log.error('Adresse konnte nicht gespeichert werden. ' + addressInstance.errors)
+                            redirect(url: request.getHeader('referer'), params: params)
+                            return
+                        }
+
+                        flash.message = message(code: 'default.created.message', args: [message(code: 'address.label'), addressInstance.name])
                     }
 
-                    params.remove('type.id')
-
-                    addressInstance.properties = params
-
-                    if (!addressInstance.save()) {
-                        flash.error = message(code: 'default.save.error.general.message')
-                        log.error('Adresse konnte nicht gespeichert werden. ' + addressInstance.errors)
-                        redirect(url: request.getHeader('referer'), params: params)
-                        return
-                    }
-
-                    flash.message = message(code: 'default.created.message', args: [message(code: 'address.label'), addressInstance.name])
-                }
-              
-                redirect(url: request.getHeader('referer'), params: params)
-			break
-		}
+                    redirect(url: request.getHeader('referer'), params: params)
+                    break
+            }
+        }
     }
 
     @Secured(['ROLE_USER'])
@@ -103,79 +102,81 @@ class AddressController  {
     @DebugAnnotation(test='hasAffiliation("INST_EDITOR")')
     @Secured(closure = { principal.user?.hasAffiliation("INST_EDITOR") })
     def edit() {
-        Address addressInstance = Address.get(params.id)
-        if (! addressInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'address.label'), params.id])
-            redirect(url: request.getHeader('referer'))
-            return
-        }
-        if (! addressbookService.isAddressEditable(addressInstance, springSecurityService.getCurrentUser())) {
-            redirect(url: request.getHeader('referer'))
-            return
-        }
-        if (params.version) {
-            Long version = params.long('version')
-            if (addressInstance.version > version) {
-                addressInstance.errors.rejectValue('version', 'default.optimistic.locking.failure',
-                          [message(code: 'address.label')] as Object[],
-                          "Another user has updated this Address while you were editing")
+        Address.withTransaction {
+            Address addressInstance = Address.get(params.id)
+            if (!addressInstance) {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'address.label'), params.id])
                 redirect(url: request.getHeader('referer'))
                 return
             }
-        }
-
-        addressInstance.properties = params
-
-        List<RefdataValue> typesToRemove = []
-        addressInstance.type.each {
-            if(!(it.toString() in params.list('type.id'))){
-                typesToRemove << it
+            if (!addressbookService.isAddressEditable(addressInstance, springSecurityService.getCurrentUser())) {
+                redirect(url: request.getHeader('referer'))
+                return
             }
-        }
+            if (params.version) {
+                Long version = params.long('version')
+                if (addressInstance.version > version) {
+                    addressInstance.errors.rejectValue('version', 'default.optimistic.locking.failure',
+                            [message(code: 'address.label')] as Object[],
+                            "Another user has updated this Address while you were editing")
+                    redirect(url: request.getHeader('referer'))
+                    return
+                }
+            }
 
-        typesToRemove.each {
-            addressInstance.removeFromType(it)
-        }
+            addressInstance.properties = params
 
-        params.list('type.id').each {
-                if(!(it in addressInstance.type))
-                {
+            List<RefdataValue> typesToRemove = []
+            addressInstance.type.each {
+                if (!(it.toString() in params.list('type.id'))) {
+                    typesToRemove << it
+                }
+            }
+
+            typesToRemove.each {
+                addressInstance.removeFromType(it)
+            }
+
+            params.list('type.id').each {
+                if (!(it in addressInstance.type)) {
                     addressInstance.addToType(RefdataValue.get(Long.parseLong(it)))
                 }
-        }
+            }
 
+            if (!addressInstance.save()) {
+                redirect(url: request.getHeader('referer'))
+                return
+            }
 
-        if (! addressInstance.save()) {
+            flash.message = message(code: 'default.updated.message', args: [message(code: 'address.label'), (addressInstance.name ?: '')])
             redirect(url: request.getHeader('referer'))
-            return
         }
-
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'address.label'), (addressInstance.name ?: '')])
-        redirect(url: request.getHeader('referer'))
     }
 
     @DebugAnnotation(test='hasAffiliation("INST_EDITOR")')
     @Secured(closure = { principal.user?.hasAffiliation("INST_EDITOR") })
     def delete() {
-        Address addressInstance = Address.get(params.id)
-        if (! addressInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'address.label'), params.id])
-            redirect action: 'list'
-            return
-        }
-        if (! addressbookService.isAddressEditable(addressInstance, springSecurityService.getCurrentUser())) {
-            redirect action: 'show', id: params.id
-            return
-        }
+        Address.withTransaction {
+            Address addressInstance = Address.get(params.id)
+            if (!addressInstance) {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'address.label'), params.id])
+                redirect action: 'list'
+                return
+            }
+            if (!addressbookService.isAddressEditable(addressInstance, springSecurityService.getCurrentUser())) {
+                redirect action: 'show', id: params.id
+                return
+            }
 
-        try {
-            addressInstance.delete(flush: true)
-			flash.message = message(code: 'default.deleted.message', args: [message(code: 'address.label'), params.id])
-            redirect action: 'list'
-        }
-        catch (DataIntegrityViolationException e) {
-			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'address.label'), params.id])
-            redirect action: 'show', id: params.id
+            try {
+                addressInstance.delete()
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'address.label'), params.id])
+                redirect action: 'list'
+            }
+            catch (DataIntegrityViolationException e) {
+                flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'address.label'), params.id])
+                redirect action: 'show', id: params.id
+            }
         }
     }
 }
