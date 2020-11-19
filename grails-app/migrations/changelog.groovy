8ade8e2ffbe72386fe3a528fe021ee9547cb4e7e
David Klober <david.klober@hbz-nrw.de>
2020-11-19 11:02:03 +0100

    dbm: timestamp with time zone

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -112,4 +112,5 @@ databaseChangeLog = {
     include file: 'changelog-2020-11-04.groovy'
     include file: 'changelog-2020-11-09.groovy'
     include file: 'changelog-2020-11-16.groovy'
+    include file: 'changelog-2020-11-18.groovy'
 }
