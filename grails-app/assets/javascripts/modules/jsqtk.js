07997582a1689703b64584115c0b4de8518256e7
David Klober <david.klober@hbz-nrw.de>
2020-11-25 09:16:40 +0100

    improved jsqtk

--- grails-app/assets/javascripts/modules/jsqtk.js
+++ grails-app/assets/javascripts/modules/jsqtk.js
@@ -6,68 +6,95 @@ var jsqtk = {
     keys: [],
     idCounter: 0,
 
-    info: function() {
-        let evsCounter = 0
-        let output = '--- EventListener found (min. 2 of type / element) ---\n\n'
+    elem: function (jsqtk_id) {
+        let elem = $('*[data-jsqtk-id="' + jsqtk_id + '"]')
 
-        $.each($('*'), function(i, elem){
+        if (elem) {
+            console.log(elem)
+            let evs = $._data(elem[0], 'events')
+            $.each(evs, function (i, elist) {
+                $.each(elist, function (ii, oo) {
+                    console.log(oo.handler)
+                })
+            })
+        }
+    },
+
+    history: function () {
+        $.each(jsqtk.keys, function (i, e) {
+            console.log(e)
+        })
+    },
+
+    _check: function (events) {
+        let result = []
+
+        for (let i=0; i<events.length; i++) {
+            for (let j=i+1; j<events.length; j++) {
+                if (events[i].handler.toString() == events[j].handler.toString()) {
+                    if ($.inArray(events[i].handler, result) < 0) {
+                        result.push(events[i].handler)
+                    }
+                    if ($.inArray(events[j].handler, result) < 0) {
+                        result.push(events[j].handler)
+                    }
+                }
+            }
+        }
+        return result
+    },
+
+    go: function () {
+        let evsCounter = 0
+
+        $.each($('*'), function (i, elem) {
             jsqtk.idCounter = jsqtk.idCounter + 1
 
-            let jsqtk_eid = 'jsqtk-' + jsqtk.idCounter
+            let jsqtkEid = 'jsqtk-' + jsqtk.idCounter
             let evs = $._data(elem, 'events')
 
             if (evs) {
                 evsCounter = evsCounter + Object.keys(evs).length
-                let isCandidate = false
 
-                let info = elem.nodeName + ' [' + jsqtk_eid + ']'
-                if (elem.id) { info = info + ' id="' + elem.id + '"' }
-                if (elem.className) { info = info + ' class="' + elem.className + '"' }
+                $.each(evs, function (ii, elist) {
+                    if (elist.length > 1) {
+                        let checkList = jsqtk._check(elist)
 
-                $.each(evs, function(ii, e){
-                    $.each(e, function(ii, oo){
-                        info = info + ' @ ' + oo.type + ':' + oo.guid
-
-                        if (e.length > 1){
-                            isCandidate = true
+                        if (checkList.length > 0) {
+                            let eType
 
                             if ($(elem).attr('data-jsqtk-id')) {
-                                jsqtk_eid = $(elem).attr('data-jsqtk-id')
+                                jsqtkEid = $(elem).attr('data-jsqtk-id')
                             } else {
-                                $(elem).attr('data-jsqtk-id', jsqtk_eid)
+                                $(elem).attr('data-jsqtk-id', jsqtkEid)
                             }
 
-                            if (! jsqtk.result[jsqtk_eid]) {
-                                jsqtk.result[jsqtk_eid] = [elem]
+                            if (! jsqtk.result[jsqtkEid]) {
+                                jsqtk.result[jsqtkEid] = [elem]
                             }
-                            jsqtk.result[jsqtk_eid].push(oo)
+
+                            jsqtk.result[jsqtkEid][eType] = checkList
                         }
-                    })
+                    }
                 })
-
-                if (isCandidate) {
-                    output = output + info + '\n'
-                }
             }
         })
 
-        let keys = Object.keys(jsqtk.result).sort(function(a, b){
+        let keys = Object.keys(jsqtk.result).sort(function (a, b) {
             a.split('-')[1] < b.split('-')[1] ? 1 : -1
         })
         jsqtk.keys.push(keys)
 
         console.log('[jsqtk] event listener found: ' + evsCounter)
         console.log('[jsqtk] candidates found: ' + Object.keys(jsqtk.result).length)
-        console.log('[jsqtk] candidate keys: ' + keys)
+        console.log('[jsqtk] keys: ' + keys)
 
-        // console.log( output )
-
-        keys.forEach( function(k){
+        keys.forEach(function (k) {
             console.log(jsqtk.result[k])
         })
     }
 }
 
-console.log('jsqtk loaded .. run jsqtk.info()')
+console.log('[jsqtk] loaded .. use jsqtk.go() / jsqtk.elem(jsqtk-id) / jsqtk.history() ')
 
 
