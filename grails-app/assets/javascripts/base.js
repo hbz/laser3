5adda86ca6da6935152e00290f14f1c31238d62b
David Klober <david.klober@hbz-nrw.de>
2020-12-17 12:40:14 +0100

    prepared assets minification

--- grails-app/assets/javascripts/base.js
+++ grails-app/assets/javascripts/base.js
@@ -1,12 +1,10 @@
 // FileName: base.js
 //
-//= require libs/jquery-3.2.1.min.js
+//= require libs/jquery-3.5.1.js
 //= require libs/jquery.poshytip.js
-//= require libs/jquery-editable-poshytip.min.js
-//= require libs/jquery.readmore.min.js
-//= require libs/moment-with-locales.min.js
-//= require libs/moment.min.js
-//= require libs/readmore.min.js
-//= require /select2/js/select2.min.js      // vendor
+//= require libs/jquery-editable-poshytip.js
+//= require libs/moment-with-locales.js
+//= require libs/readmore.js
+//= require /select2/js/select2.js      // vendor
 
 console.log('+ base.js')
