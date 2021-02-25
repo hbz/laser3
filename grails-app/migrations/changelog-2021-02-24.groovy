65aeda8af639bce9dda6e1f18f33e72b540e8da0
David Klober <david.klober@hbz-nrw.de>
2021-02-25 11:44:01 +0100

    qa dbm

--- grails-app/migrations/changelog-2021-02-24.groovy
+++ grails-app/migrations/changelog-2021-02-24.groovy
@@ -27,7 +27,7 @@ update org_setting
     changeSet(author: "klober (modified)", id: "1614160052993-3") {
         grailsChange {
             change {
-                sql.execute("delete from refdata_value where rdv_value = 'Department' and rdv_owner = (select rdc_id from refdata_category where rdc_description = 'combo.type')")
+                sql.execute("delete from refdata_value where rdv_value='Department' and rdv_owner = (select rdc_id from refdata_category where rdc_description='combo.type')")
             }
             rollback {}
         }
@@ -35,9 +35,32 @@ update org_setting
     changeSet(author: "klober (modified)", id: "1614160052993-4") {
         grailsChange {
             change {
-                sql.execute("delete from refdata_value where rdv_value = 'Licensee_Collective' and rdv_owner = (select rdc_id from refdata_category where rdc_description = 'organisational.role')")
-                sql.execute("delete from refdata_value where rdv_value = 'Subscriber_Collective' and rdv_owner = (select rdc_id from refdata_category where rdc_description = 'organisational.role')")
-                sql.execute("delete from refdata_value where rdv_value = 'Subscription Collective' and rdv_owner = (select rdc_id from refdata_category where rdc_description = 'organisational.role')")
+                String rdc_query = "(select rdc_id from refdata_category where rdc_description='organisational.role')"
+
+                sql.execute("delete from org_role where or_roletype_fk = (select rdv_id from refdata_value where rdv_value = 'Licensee_Collective' and rdv_owner = " + rdc_query + ")")
+                sql.execute("delete from refdata_value where rdv_value='Licensee_Collective' and rdv_owner = " + rdc_query)
+            }
+            rollback {}
+        }
+    }
+    changeSet(author: "klober (modified)", id: "1614160052993-5") {
+        grailsChange {
+            change {
+                String rdc_query = "(select rdc_id from refdata_category where rdc_description = 'organisational.role')"
+
+                sql.execute("delete from org_role where or_roletype_fk = (select rdv_id from refdata_value where rdv_value='Subscriber_Collective' and rdv_owner = " + rdc_query + ")")
+                sql.execute("delete from refdata_value where rdv_value='Subscriber_Collective' and rdv_owner = " + rdc_query)
+            }
+            rollback {}
+        }
+    }
+    changeSet(author: "klober (modified)", id: "1614160052993-6") {
+        grailsChange {
+            change {
+                String rdc_query = "(select rdc_id from refdata_category where rdc_description = 'organisational.role')"
+
+                sql.execute("delete from org_role where or_roletype_fk = (select rdv_id from refdata_value where rdv_value='Subscription Collective' and rdv_owner = " + rdc_query + ")")
+                sql.execute("delete from refdata_value where rdv_value='Subscription Collective' and rdv_owner = " + rdc_query)
             }
             rollback {}
         }
