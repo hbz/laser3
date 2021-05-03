0c8561eeb6367acef13932aee72611a55104c014
David Klober <david.klober@hbz-nrw.de>
2021-05-03 14:13:52 +0200

    ..

--- src/main/groovy/de/laser/reporting/myInstitution/LicenseConfig.groovy
+++ src/main/groovy/de/laser/reporting/myInstitution/LicenseConfig.groovy
@@ -63,48 +63,6 @@ class LicenseConfig extends BaseConfig {
                     ]
             ],
 
-            /*member : [
-                    meta : [
-                            class: Org
-                    ],
-                    source : [
-                            'depending-member' : 'Alle betroffenen Teilnehmer'
-                    ],
-                    fields : [
-                            'country'           : FIELD_TYPE_REFDATA,
-                            'customerType'      : FIELD_TYPE_CUSTOM_IMPL,
-                            'eInvoice'          : FIELD_TYPE_PROPERTY,
-                            'funderHskType'     : FIELD_TYPE_REFDATA,
-                            'funderType'        : FIELD_TYPE_REFDATA,
-                            'legalInfo'         : FIELD_TYPE_CUSTOM_IMPL,
-                            'libraryNetwork'    : FIELD_TYPE_REFDATA,
-                            'libraryType'       : FIELD_TYPE_REFDATA,
-                            'orgType'           : FIELD_TYPE_REFDATA_JOINTABLE,
-                            //'region'            : FIELD_TYPE_REFDATA,
-                            'subjectGroup'      : FIELD_TYPE_REFDATA_JOINTABLE
-                    ],
-                    filter : [
-                            default: [
-                                    [ 'country', 'subjectGroup', 'libraryType' ],
-                                    [ 'libraryNetwork', 'funderType', 'funderHskType' ],
-                                    [ 'orgType', 'eInvoice' ],
-                                    [ 'customerType', 'legalInfo' ]
-                            ]
-                    ],
-                    query : [
-                            'Teilnehmer' : [
-                                    'member-orgType'            : 'Organisationstyp',
-                                    'member-customerType'       : 'Kundentyp',
-                                    'member-libraryType'        : 'Bibliothekstyp',
-                                    'member-region'             : 'Bundesländer',
-                                    'member-subjectGroup'       : 'Fächergruppen',
-                                    'member-libraryNetwork'     : 'Verbundzugehörigkeit',
-                                    'member-funderType'         : 'Unterhaltsträger',
-                                    'member-funderHskType'      : 'Trägerschaft'
-                            ]
-                    ]
-            ],*/
-
             licensor : [
                     meta : [
                             class: Org
