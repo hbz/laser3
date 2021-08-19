64bad12bccd7ea0e2fd759082292deb2502cd9d7
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2021-08-19 10:25:19 +0200

    ERMS-3534: SurveyResult finishDate löschen

---
+++ grails-app/migrations/changelog-2021-08-19.groovy
@@ -0,0 +1,7 @@
+databaseChangeLog = {
+
+    changeSet(author: "djebeniani (generated)", id: "1629361167063-1") {
+        dropColumn(columnName: "surre_finish_date", tableName: "survey_result")
+    }
+
+}
