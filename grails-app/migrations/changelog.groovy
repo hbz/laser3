ccc4b1260a82cb9684ad3ee5746566e82b91582d
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2021-08-19 10:48:36 +0200

    ERMS-3534: SurveyResult finishDate löschen

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -150,4 +150,5 @@ databaseChangeLog = {
 	include file: 'changelog-2021-07-27.groovy'
 	include file: 'changelog-2021-08-02.groovy'
     include file: 'changelog-2021-08-11.groovy'
+	include file: 'changelog-2021-08-19.groovy'
 }
