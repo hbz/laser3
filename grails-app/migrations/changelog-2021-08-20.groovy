265625f08bd5f7a3e8bc3c7190397c3911eedf79
David Klober <david.klober@hbz-nrw.de>
2021-08-24 08:16:39 +0200

    dbm

--- grails-app/migrations/changelog-2021-08-20.groovy
+++ grails-app/migrations/changelog-2021-08-20.groovy
@@ -30,4 +30,23 @@ databaseChangeLog = {
         }
     }
 
+    changeSet(author: "klober (modified)", id: "1629440898005-5") {
+        grailsChange {
+            change {
+                sql.execute("delete from refdata_value where rdv_value = 'type_0' and rdv_owner = (select rdc_id from refdata_category where rdc_description = 'workflow.condition.type')")
+            }
+            rollback {}
+        }
+    }
+    changeSet(author: "klober (modified)", id: "1629440898005-6") {
+        grailsChange {
+            change {
+                sql.execute('update wf_condition set wfc_type = 4 where wfc_type = 0 and wfc_file1 is null')
+                sql.execute('update wf_condition set wfc_type = 7 where wfc_type = 0 and wfc_file1 is not null')
+                sql.execute('update wf_condition_prototype set wfcp_type = 4 where wfcp_type = 0 and wfcp_file1 is null')
+                sql.execute('update wf_condition_prototype set wfcp_type = 7 where wfcp_type = 0 and wfcp_file1 is not null')
+            }
+            rollback {}
+        }
+    }
 }
