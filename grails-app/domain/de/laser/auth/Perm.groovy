6068db6ae751d95533c0ee46bc6b71e5dca50343
agalffy <andrisgalffy@gmail.com>
2021-10-06 13:31:14 +0200

    documentation-erms-3903
    first batch of domain classes (package de.laser.auth) documented with GroovyDoc, along with code quality fixes and deprecation markings

--- grails-app/domain/de/laser/auth/Perm.groovy
+++ grails-app/domain/de/laser/auth/Perm.groovy
@@ -1,8 +1,11 @@
 package de.laser.auth
 
+/**
+ * Class to retain permissions to role types; this ensures the permission cascade for users and orgs
+ */
 class Perm {
 
-    String code
+    String code //the perm being granted
 
     static mapping = {
         cache   true
