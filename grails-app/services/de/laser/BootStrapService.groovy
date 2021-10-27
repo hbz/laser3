5a63d698a5be8c9d22d50a7e4da2f7eabaa99c85
agalffy <andrisgalffy@gmail.com>
2021-10-27 14:20:05 +0200

    documentation-erms-3903
    further documentation as of ERMS-3903, this time, the i10n and bootstrapping methods

--- grails-app/services/de/laser/BootStrapService.groovy
+++ grails-app/services/de/laser/BootStrapService.groovy
@@ -14,6 +14,9 @@ import groovy.sql.Sql
 import org.hibernate.SQLQuery
 import org.hibernate.type.TextType
 
+/**
+ * This service encapsulates methods called upon system startup; it defines system-wide constants, updates hard-coded translations and sets other globally relevant parameters
+ */
 @Transactional
 class BootStrapService {
 
@@ -26,8 +29,12 @@ class BootStrapService {
     def sessionFactory
     def userService
 
-    final static BOOTSTRAP = true   // indicates this object is created via bootstrap
+    final static BOOTSTRAP = true   // indicates this object is created via bootstrap (= is hard-coded in system, persists database resets and instances)
 
+    /**
+     * Runs initialisation and triggers other startup methods
+     * @param servletContext unused
+     */
     void init(def servletContext) {
 
         ConfigUtils.checkConfig()
@@ -136,8 +143,15 @@ class BootStrapService {
         log.debug("           \\/     \\/     \\/              \\/ ")
     }
 
+    /**
+     * Destructor method
+     */
     void destroy() {}
 
+    /**
+     * Sets - if not exists - one or more system users with global roles and a fallback anonymous user if all users have been deleted. The system users are
+     * defined in the server config (see laser2-config example) file which should be stored on each server instance / locale instance separately
+     */
     void setupSystemUsers() {
 
         // Create anonymousUser that serves as a replacement when users are deleted
@@ -212,6 +226,12 @@ class BootStrapService {
         }
     }
 
+    /**
+     * This setup should hold only for the QA environment and is disused as everyone should set up its own environment. Was used to create demo organisations and to assign each
+     * hbz group member to them for demonstration and testing purposes.
+     * @see UserService#setupAdminAccounts()
+     * @see OrganisationService#createOrgsFromScratch()
+     */
     void setupAdminUsers() {
 
         if (ServerUtils.getCurrentServer() == ServerUtils.SERVER_QA) {
@@ -238,6 +258,13 @@ class BootStrapService {
         }
     }
 
+    /**
+     * Sets up the global and institutional roles and grants the permissions to them
+     * @see Role
+     * @see Perm
+     * @see PermGrant
+     * @see UserOrg
+     */
     void setupRolesAndPermissions() {
 
         PermGrant.executeUpdate('delete PermGrant pg')
@@ -314,6 +341,12 @@ class BootStrapService {
         createOrgPerms(orgConsortiumRole,           ['ORG_CONSORTIUM'])
     }
 
+    /**
+     * Parses the given CSV file path according to the file header reference specified by objType
+     * @param filePath the source file to parse
+     * @param objType the object type reference; this is needed to read the definitions in the columns correctly and is one of RefdataCategory, RefdataValue or PropertyDefinition
+     * @return the {@link List} of rows (each row parsed as {@link Map}) retrieved from the source file
+     */
     List getParsedCsvData(String filePath, String objType) {
 
         List result = []
@@ -389,6 +422,9 @@ class BootStrapService {
         result
     }
 
+    /**
+     * Creates or updates stored database functions. They are located in the /grails-app/migrations/functions folder
+     */
     void updatePsqlRoutines() {
 
         try {
@@ -429,12 +465,19 @@ class BootStrapService {
         }
     }
 
+    /**
+     * Ensures database permissions for the backup and readonly users
+     */
     void adjustDatabasePermissions() {
 
         Sql sql = new Sql(dataSource)
         sql.rows("SELECT * FROM grants_for_maintenance()")
     }
 
+    /**
+     * @deprecated is replaced by {@link #setupPropertyDefinitions}
+     */
+    @Deprecated
     void createPropertyDefinitionsWithI10nTranslations(requiredProps) {
 
         requiredProps.each { default_prop ->
@@ -459,6 +502,11 @@ class BootStrapService {
         }
     }
 
+    /**
+     * Assigns to the given role the given permission
+     * @param role the {@link Role} whose permissions should be granted
+     * @param perm the {@link Perm} permission to be granted
+     */
     void ensurePermGrant(Role role, Perm perm) {
         PermGrant existingPermGrant = PermGrant.findByRoleAndPerm(role,perm)
         if (! existingPermGrant) {
@@ -473,6 +521,7 @@ class BootStrapService {
     /**
      * RefdataValue.group is used only for OrgRole to filter the types of role available in 'Add Role' action
      * This is done by providing 'linkType' (using instance class) to the '_orgLinksModal' template.
+     * This method sets those (actually never used) reference groups
      */
     void setOrgRoleGroups() {
         String lic = License.name
@@ -506,6 +555,11 @@ class BootStrapService {
         }
     }
 
+    /**
+     * Processes the hard coded reference value sources and updates the reference values and their categories
+     * @see RefdataValue
+     * @see RefdataCategory
+     */
     void setupRefdata() {
 
         List rdcList = getParsedCsvData('setup/RefdataCategory.csv', 'RefdataCategory')
@@ -521,6 +575,10 @@ class BootStrapService {
         }
     }
 
+    /**
+     * Processes the hard coded property definition source and updates the property definitions
+     * @see PropertyDefinition
+     */
     void setupPropertyDefinitions() {
 
         List pdList = getParsedCsvData('setup/PropertyDefinition.csv', 'PropertyDefinition')
@@ -530,6 +588,7 @@ class BootStrapService {
         }
     }
 
+    @Deprecated
     void setupOnixPlRefdata() {
 
         // Refdata values that need to be added to the database to allow ONIX-PL licenses to be compared properly. The code will
@@ -621,6 +680,7 @@ class BootStrapService {
         // log.debug("new gokb record source: ${gokb_record_source}")
     }
 
+    @Deprecated
     void setupContentItems() {
 
         // The default template for a property change on a title
@@ -657,6 +717,11 @@ No Host Platform URL Content
 ''')
     }
 
+    /**
+     * This is the hard coded list of identifier namespace definitions; the method creates or updates the namespaces according to those entries.
+     * Beware: description_de and description_en are nullable but not blank!
+     * @see IdentifierNamespace
+     */
     void setIdentifierNamespace() {
 
         //TODO isUnique/isHidden flags are set provisorically to "false", adaptations may be necessary
