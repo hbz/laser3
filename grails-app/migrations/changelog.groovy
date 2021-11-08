3bd542eac9fec28dbcd7a221b703faf7cfa0ac15
David Klober <david.klober@hbz-nrw.de>
2021-11-08 09:57:59 +0100

    ..

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -159,6 +159,6 @@ databaseChangeLog = {
     include file: 'changelog-2021-09-02.groovy'
     include file: 'changelog-2021-09-21.groovy'
     include file: 'changelog-2021-09-27.groovy'
-    include file: 'changelog-2021-11-04.groovy'
     include file: 'changelog-2021-10-07.groovy'
+	include file: 'changelog-2021-11-04.groovy'
 }
