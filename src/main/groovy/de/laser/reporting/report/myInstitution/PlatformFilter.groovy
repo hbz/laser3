4f38f6dfe5a2030d4d4d1f566a57d31217598339
David Klober <david.klober@hbz-nrw.de>
2022-02-14 11:05:09 +0100

    ..

--- src/main/groovy/de/laser/reporting/report/myInstitution/PlatformFilter.groovy
+++ src/main/groovy/de/laser/reporting/report/myInstitution/PlatformFilter.groovy
@@ -133,13 +133,11 @@ class PlatformFilter extends BaseFilter {
                         filterLabelValue = RefdataValue.get(params.long(key)).getI10n('value')
                     }
                     else if (p == BaseConfig.CUSTOM_IMPL_KEY_PLT_PACKAGE_STATUS) {
-                        queryParts.add('Subscription sub')
-
                         queryParts.add('Package pkg')
-                        whereParts.add('pkg.nominalPlatform = plt')
-                        whereParts.add('pkg.packageStatus.id = :p' + (++pCount))
+                        whereParts.add('pkg.nominalPlatform = plt and pkg.packageStatus.id = :p' + (++pCount))
                         queryParams.put('p' + pCount, params.long(key))
 
+                        queryParts.add('Subscription sub')
                         queryParts.add('SubscriptionPackage subPkg')
                         whereParts.add('subPkg.subscription = sub and subPkg.pkg = pkg')
 
@@ -152,12 +150,12 @@ class PlatformFilter extends BaseFilter {
                         filterLabelValue = RefdataValue.get(params.long(key)).getI10n('value')
                     }
                     else if (p == BaseConfig.CUSTOM_IMPL_KEY_PLT_SUBSCRIPTION_STATUS) {
+                        queryParts.add('Package pkg')
+                        whereParts.add('pkg.nominalPlatform = plt')
+
                         queryParts.add('Subscription sub')
                         whereParts.add('sub.status.id = :p' + (++pCount))
                         queryParams.put('p' + pCount, params.long(key))
-
-                        queryParts.add('Package pkg')
-                        whereParts.add('pkg.nominalPlatform = plt')
 
                         queryParts.add('SubscriptionPackage subPkg')
                         whereParts.add('subPkg.subscription = sub and subPkg.pkg = pkg')
