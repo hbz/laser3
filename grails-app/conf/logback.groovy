d80f0d7539bd87e9f56b755d14f20aa913587d3e
David Klober <david.klober@hbz-nrw.de>
2022-03-10 13:20:02 +0100

    first configuration

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,37 +1,83 @@
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.filter.Filter
+import ch.qos.logback.core.spi.FilterReply
 import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
-import java.nio.charset.StandardCharsets
+import java.nio.charset.Charset
+import java.text.SimpleDateFormat
 
 conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
 
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
+// -- filter
+
+class ShrinkFilter extends Filter<ILoggingEvent> {
+
+    @Override
+    FilterReply decide(ILoggingEvent event) {
+        if (event.getMessage().contains("HHH020003")) {
+            return FilterReply.DENY
+        }
+        if (event.getMessage().contains("HHH020008")) {
+            return FilterReply.DENY
+        }
+        if (event.getMessage().contains("Replaced rule for ")) {
+            return FilterReply.DENY
+        }
+        return FilterReply.ACCEPT
+    }
+}
+
+// -- appender
+
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        charset = StandardCharsets.UTF_8
+        charset = Charset.forName('UTF-8')
 
         pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
+            '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' +
+                    '%clr(%5p) ' + // Log level
+                    '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                    '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                    '%m%n%wex'
     }
 }
 
-def targetDir = BuildSettings.TARGET_DIR
+String targetDir = "${BuildSettings.BASE_DIR}/logs"
+String date = "${(new SimpleDateFormat("yyyy-MM-dd")).format(new Date())}"
+
 if (Environment.isDevelopmentMode() && targetDir != null) {
+
     appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
+        file = "${targetDir}/errors.log"
         append = true
         encoder(PatternLayoutEncoder) {
-            charset = StandardCharsets.UTF_8
-            pattern = "%level %logger - %msg%n"
+            pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} | %level %logger - %msg%n"
         }
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    appender("LOGFILE", FileAppender) {
+        file = "${targetDir}/${date}.log"
+        append = true
+        filter(ShrinkFilter)
+        encoder(PatternLayoutEncoder) {
+            pattern = "%d{HH:mm:ss.SSS} | %level %logger - %nopex%msg%n"
+        }
+    }
+
+    logger("StackTrace",    ERROR, ['FULL_STACKTRACE'], false)
+
+    logger("de.laser",      DEBUG, ['LOGFILE'], false)
+    logger("com.k_int",     DEBUG, ['LOGFILE'], false)
+
+    root(INFO, ['LOGFILE'])
 }
-root(ERROR, ['STDOUT'])
+
+logger("de.laser",      DEBUG, ['STDOUT'], false)
+logger("com.k_int",     DEBUG, ['STDOUT'], false)
+
+root(INFO, ['STDOUT'])
+
+
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,85 @@
-// Place your Spring DSL code here
+package spring
+
+import de.laser.custom.CustomMigrationCallbacks
+import de.laser.custom.CustomUserDetailsService
+import de.laser.custom.CustomAuthSuccessHandler
+import de.laser.custom.CustomAuditRequestResolver
+import de.laser.custom.CustomWkhtmltoxService
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider
+import org.springframework.security.web.authentication.session.CompositeSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.ConcurrentSessionControlAuthenticationStrategy
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy
+import org.springframework.security.web.context.SecurityContextPersistenceFilter
+
+import grails.plugin.springsecurity.SpringSecurityUtils
+
 beans = {
+
+    migrationCallbacks( CustomMigrationCallbacks ) {
+        grailsApplication = ref('grailsApplication')
+    }
+
+    // [ user counter ..
+    sessionRegistry( SessionRegistryImpl )
+
+    registerSessionAuthenticationStrategy( RegisterSessionAuthenticationStrategy, ref('sessionRegistry') )
+
+    sessionFixationProtectionStrategy( SessionFixationProtectionStrategy )
+
+    concurrentSessionControlAuthenticationStrategy( ConcurrentSessionControlAuthenticationStrategy, ref('sessionRegistry') ){
+        maximumSessions = -1
+        // exceptionIfMaximumExceeded = true
+    }
+
+    sessionAuthenticationStrategy( CompositeSessionAuthenticationStrategy, [
+            ref('concurrentSessionControlAuthenticationStrategy'),
+            ref('sessionFixationProtectionStrategy'),
+            ref('registerSessionAuthenticationStrategy')
+    ])
+    // .. ]
+
+    // [ supporting initMandatorySettings for users ..
+    authenticationSuccessHandler( CustomAuthSuccessHandler ) {
+        ConfigObject conf = SpringSecurityUtils.securityConfig
+
+        requestCache                = ref('requestCache')
+        redirectStrategy            = ref('redirectStrategy')
+        defaultTargetUrl            = conf.successHandler.defaultTargetUrl
+        alwaysUseDefaultTargetUrl   = conf.successHandler.alwaysUseDefault
+        targetUrlParameter          = conf.successHandler.targetUrlParameter
+        ajaxSuccessUrl              = conf.successHandler.ajaxSuccessUrl
+        useReferer                  = conf.successHandler.useReferer
+    }
+    // .. ]
+
+    // [ audit logging ..
+    auditRequestResolver( CustomAuditRequestResolver ) {
+        springSecurityService = ref('springSecurityService')
+    }
+    // .. ]
+
+    userDetailsService( CustomUserDetailsService ) {
+        grailsApplication = ref('grailsApplication')
+    }
+
+    userDetailsByNameServiceWrapper( UserDetailsByNameServiceWrapper ) {
+        userDetailsService = ref('userDetailsService')
+    }
+
+    preAuthenticatedAuthenticationProvider( PreAuthenticatedAuthenticationProvider ) {
+        preAuthenticatedUserDetailsService = ref('userDetailsByNameServiceWrapper')
+    }
+
+    securityContextPersistenceFilter( SecurityContextPersistenceFilter )
+
+    // [ wkhtmltopdf ..
+
+    wkhtmltoxService( CustomWkhtmltoxService ) {
+        grailsApplication = ref('grailsApplication')
+        mailMessageContentRenderer = ref('mailMessageContentRenderer')
+    }
+    // .. ]
 }
