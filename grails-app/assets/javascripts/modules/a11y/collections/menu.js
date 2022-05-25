3b4c6c10b24f41c02ee451a10ce7618c3c5733e1
bluoss <ingrid.bluoss@hbz-nrw.de>
2022-05-25 10:12:58 +0200

    ERMS-4244

--- grails-app/assets/javascripts/modules/a11y/collections/menu.js
+++ grails-app/assets/javascripts/modules/a11y/collections/menu.js
@@ -13,9 +13,9 @@ a11yMenu = {
 
         // for Main Menu
         $('#mainMenue .dropdown').dropdown({
-            action: function(text, value, element) {
+/*            action: function(text, value, element) {
                 element.click();
-            },
+            },*/
             selectOnKeydown        : false,
             on: 'hover',
             onHide         : function() {
@@ -28,9 +28,9 @@ a11yMenu = {
 
         // for Test Main Menu on dev/index view
         $('#mainMenueTest .dropdown').dropdown({
-            action: function(text, value, element) {
+/*            action: function(text, value, element) {
                 element.click();
-            },
+            },*/
             selectOnKeydown        : false,
             on: 'hover',
             onHide         : function() {
