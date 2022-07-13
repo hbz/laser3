1e8da90a02346b5117c1531cf165b684865b98df
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2022-07-13 17:47:39 +0200

    ERMS-4039: 500er bei Meine Lizenzverwaltung / Dokumente anhängen

--- grails-app/services/de/laser/ManagementService.groovy
+++ grails-app/services/de/laser/ManagementService.groovy
@@ -26,6 +26,8 @@ import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.transaction.TransactionStatus
 
 import javax.servlet.http.HttpServletRequest
+import java.nio.file.Files
+import java.nio.file.Path
 import java.text.SimpleDateFormat
 import java.util.concurrent.ExecutorService
 
@@ -809,6 +811,7 @@ class ManagementService {
         //Is be to need, because with upload_file the formService.validateToken(params) is not working really
         params.remove('upload_file')
         def input_stream = input_file.inputStream
+        File sourceFile
         if(result.editable && formService.validateToken(params)) {
             Locale locale = LocaleContextHolder.getLocale()
             FlashScope flash = getCurrentFlashScope()
@@ -816,7 +819,7 @@ class ManagementService {
             if (selectedSubs) {
                 Set<Subscription> subscriptions = Subscription.findAllByIdInList(selectedSubs)
                     if(params.processOption == 'newDoc') {
-                        subscriptions.each { Subscription subscription ->
+                        subscriptions.eachWithIndex { Subscription subscription, int status ->
                             if (subscription.isEditableBy(result.user)) {
                                 if (input_stream) {
                                     Doc doc_content = new Doc(
@@ -830,7 +833,6 @@ class ManagementService {
 
                                     doc_content.save()
 
-                                    File new_File
                                     try {
                                         String fPath = ConfigUtils.getDocumentStorageLocation() ?: '/tmp/laser'
                                         String fName = doc_content.uuid
@@ -839,9 +841,16 @@ class ManagementService {
                                         if (!folder.exists()) {
                                             folder.mkdirs()
                                         }
-                                        new_File = new File("${fPath}/${fName}")
 
-                                        input_file.transferTo(new_File)
+                                        if(status == 0){
+                                            sourceFile = new File("${fPath}/${fName}")
+                                            input_file.transferTo(sourceFile)
+                                        }else {
+                                            Path source = sourceFile.toPath()
+                                            Path target = new File("${fPath}/${fName}").toPath()
+                                            Files.copy(source, target)
+                                        }
+
                                     }
                                     catch (Exception e) {
                                         e.printStackTrace()
