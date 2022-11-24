8d17abdd549c3cc57bd36a8f0796c6caa49f57d4
galffy <andreas.galffy@hbz-nrw.de>
2022-11-24 15:21:36 +0100

    import fix

--- grails-app/services/de/laser/SubscriptionService.groovy
+++ grails-app/services/de/laser/SubscriptionService.groovy
@@ -15,10 +15,9 @@ import de.laser.properties.PropertyDefinition
 import de.laser.properties.PropertyDefinitionGroup
 import de.laser.properties.PropertyDefinitionGroupBinding
 import de.laser.remote.ApiSource
-import de.laser.stats.Counter4Report
-import de.laser.stats.Counter5Report
 import de.laser.storage.RDConstants
 import de.laser.storage.RDStore
+import de.laser.survey.SurveyConfig
 import de.laser.utils.DateUtils
 import de.laser.utils.LocaleUtils
 import de.laser.utils.SwissKnife
