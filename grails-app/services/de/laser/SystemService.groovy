f71fe47a4e460fa7d50b2c96051b49d3619d6509
David Klober <david.klober@hbz-nrw.de>
2022-12-16 09:01:08 +0100

    erms-4706

--- grails-app/services/de/laser/SystemService.groovy
+++ grails-app/services/de/laser/SystemService.groovy
@@ -15,7 +15,7 @@ import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.action.index.IndexResponse
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
-import org.elasticsearch.common.xcontent.XContentType
+import org.elasticsearch.xcontent.XContentType
 
 import java.text.SimpleDateFormat
 
