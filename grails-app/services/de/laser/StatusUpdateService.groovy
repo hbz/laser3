d35cbe94ca9493e9a71249bf5dd0b2684d38088d
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2023-01-19 13:41:25 +0100

    Missing Import

--- grails-app/services/de/laser/StatusUpdateService.groovy
+++ grails-app/services/de/laser/StatusUpdateService.groovy
@@ -5,6 +5,7 @@ import de.laser.finance.PriceItem
 import de.laser.storage.RDStore
 import de.laser.base.AbstractLockableService
 import de.laser.system.SystemEvent
+import de.laser.utils.DateUtils
 import grails.converters.JSON
 import grails.gorm.transactions.Transactional
 
