16675cfa647c0285a221b09ebd6742ec45e0432d
David Klober <david.klober@hbz-nrw.de>
2023-02-03 14:28:41 +0100

    erms-4798

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -73,4 +73,5 @@ databaseChangeLog = {
     include file: 'changelogs/2022-12-14.groovy'
     include file: 'changelogs/2022-12-19.groovy'
     include file: 'changelogs/2022-12-22.groovy'
+    include file: 'changelogs/2023-02-03.groovy'
 }
