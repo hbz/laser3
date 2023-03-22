d5ff4d10a6a7efc546581564e7d76bf5ec8b11f3
David Klober <david.klober@hbz-nrw.de>
2023-03-22 10:14:32 +0100

    ..

--- grails-app/domain/de/laser/License.groovy
+++ grails-app/domain/de/laser/License.groovy
@@ -4,6 +4,7 @@ import de.laser.annotations.RefdataInfo
 import de.laser.auth.Role
 import de.laser.auth.User
 import de.laser.base.AbstractBaseWithCalculatedLastUpdated
+import de.laser.CustomerTypeService
 import de.laser.interfaces.CalculatedType
 import de.laser.interfaces.Permissions
 import de.laser.interfaces.ShareSupport
