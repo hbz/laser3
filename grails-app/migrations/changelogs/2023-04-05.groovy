fd93f5e25d40d2b2db6b3c4443fdb974d94e6330
MDjeb <moetez.djebeniani@hbz-nrw.de>
2023-04-05 10:30:34 +0200

    Update 2023-04-05.groovy

--- grails-app/migrations/changelogs/2023-04-05.groovy
+++ grails-app/migrations/changelogs/2023-04-05.groovy
@@ -2,7 +2,7 @@ package changelogs
 
 databaseChangeLog = {
 
-    changeSet(author: "galffy (hand-coded)", id: "1680680702702-1") {
+    changeSet(author: "djebeniani (hand-coded)", id: "1680680702702-1") {
         grailsChange {
             change {
                 sql.execute("update property_definition set pd_tenant_fk = null where pd_tenant_fk = 1 and pd_description = 'Subscription Property' and pd_name = 'Steuersatz'")
