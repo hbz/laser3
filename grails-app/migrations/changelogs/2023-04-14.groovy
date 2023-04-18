212d3e4192ea7b8f5007b24e99c1b208f4230af3
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2023-04-18 17:19:19 +0200

    Delete PlatformAccessMethod

--- grails-app/migrations/changelogs/2023-04-14.groovy
+++ grails-app/migrations/changelogs/2023-04-14.groovy
@@ -2,20 +2,22 @@ package changelogs
 
 databaseChangeLog = {
 
-    changeSet(author: "djebeniani (generated)", id: "1681469132222-7") {
-        dropForeignKeyConstraint(baseTableName: "platform_access_method", constraintName: "fke7e3f55064d7720")
+    changeSet(author: "djebeniani (generated)", id: "1681469132222-1") {
+        grailsChange {
+            change {
+                sql.execute("delete from refdata_value where rdv_value = 'vpn'")
+            }
+            rollback {}
+        }
     }
 
-    changeSet(author: "djebeniani (generated)", id: "1681469132222-6") {
-        dropForeignKeyConstraint(baseTableName: "platform_access_method", constraintName: "fke7e3f5508055d84b")
-    }
-
-    changeSet(author: "djebeniani (generated)", id: "1681469132222-5") {
-        dropUniqueConstraint(constraintName: "platform_access_method_plat_guid_key", tableName: "platform_access_method")
-    }
-
-    changeSet(author: "djebeniani (generated)", id: "1681469132222-4") {
-        dropTable(tableName: "platform_access_method")
+    changeSet(author: "djebeniani (generated)", id: "1681469132222-2") {
+        grailsChange {
+            change {
+                sql.execute("delete from org_access_point where oar_access_method_rv_fk = (select rdv_id from refdata_value where rdv_value = 'vpn')")
+            }
+            rollback {}
+        }
     }
 
     changeSet(author: "djebeniani (generated)", id: "1681469132222-3") {
@@ -27,22 +29,32 @@ databaseChangeLog = {
         }
     }
 
-
-    changeSet(author: "djebeniani (generated)", id: "1681469132222-2") {
-        grailsChange {
-            change {
-                sql.execute("delete from org_access_point where oar_access_method_rv_fk = (select rdv_id from refdata_value where rdv_value = 'vpn')")
-            }
-            rollback {}
-        }
+    changeSet(author: "djebeniani (generated)", id: "1681469132222-4") {
+        dropTable(tableName: "platform_access_method")
     }
 
-    changeSet(author: "djebeniani (generated)", id: "1681469132222-1") {
-        grailsChange {
-            change {
-                sql.execute("delete from refdata_value where rdv_value = 'vpn'")
-            }
-            rollback {}
-        }
+    changeSet(author: "djebeniani (generated)", id: "1681469132222-5") {
+        dropUniqueConstraint(constraintName: "platform_access_method_plat_guid_key", tableName: "platform_access_method")
     }
+
+    changeSet(author: "djebeniani (generated)", id: "1681469132222-6") {
+        dropForeignKeyConstraint(baseTableName: "platform_access_method", constraintName: "fke7e3f5508055d84b")
+    }
+
+    changeSet(author: "djebeniani (generated)", id: "1681469132222-7") {
+        dropForeignKeyConstraint(baseTableName: "platform_access_method", constraintName: "fke7e3f55064d7720")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
