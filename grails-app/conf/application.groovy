f7908098e53a2a43f79b8ef8ef325c895d8dbf00
David Klober <david.klober@hbz-nrw.de>
2023-05-03 08:58:31 +0200

    application.yml and more

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -12,17 +12,3 @@ grails.plugin.springsecurity.roleHierarchy = '''
     ROLE_YODA > ROLE_ADMIN
     ROLE_ADMIN > ROLE_USER
 '''
-
-// -- asynchronous-mail plugin
-asynchronous.mail.default.attempt.interval=300000l      // Five minutes
-asynchronous.mail.default.max.attempts.count=1
-asynchronous.mail.send.repeat.interval=60000l           // One minute
-asynchronous.mail.expired.collector.repeat.interval=607000l
-asynchronous.mail.messages.at.once=100
-asynchronous.mail.send.immediately=true
-asynchronous.mail.clear.after.sent=false
-asynchronous.mail.disable=false
-asynchronous.mail.useFlushOnSave=true
-asynchronous.mail.persistence.provider='hibernate5'     // Possible values are 'hibernate', 'hibernate4', 'hibernate5', 'mongodb'
-asynchronous.mail.newSessionOnImmediateSend=false
-asynchronous.mail.taskPoolSize=1
