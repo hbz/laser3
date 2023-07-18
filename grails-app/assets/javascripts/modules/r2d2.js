611c08a94fe68605859c0bdd5f7ebed1e0a21523
bluoss <ingrid.bluoss@hbz-nrw.de>
2023-07-18 12:33:41 +0200

    ERMS-5192

--- grails-app/assets/javascripts/modules/r2d2.js
+++ grails-app/assets/javascripts/modules/r2d2.js
@@ -584,6 +584,7 @@ r2d2 = {
             }
             $(href + '.ui.modal').modal({
                 onVisible: function() {
+                    tooltip.go();
                     $(this).find('.datepicker').calendar(r2d2.configs.datepicker);
                     $(this).find('.yearpicker').calendar(r2d2.configs.yearpicker);
 
