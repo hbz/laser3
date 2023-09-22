e8034e89f462eac8944012ed1084aaf9c9bf32db
galffy <andreas.galffy@hbz-nrw.de>
2023-09-22 10:50:10 +0200

    feature-erms-5277
    as of ERMS-5277, IdentifierNamespace source has been moved to external CSV file

--- src/main/groovy/de/laser/config/ConfigDefaults.groovy
+++ src/main/groovy/de/laser/config/ConfigDefaults.groovy
@@ -14,6 +14,7 @@ class ConfigDefaults {
     public static final String SETUP_REFDATA_CATEGORY_CSV      = 'setup/RefdataCategory.csv'                       // -> BootstrapService
     public static final String SETUP_REFDATA_VALUE_CSV         = 'setup/RefdataValue.csv'
     public static final String SETUP_PROPERTY_DEFINITION_CSV   = 'setup/PropertyDefinition.csv'
+    public static final String SETUP_IDENTIFIER_NAMESPACE_CSV  = 'setup/IdentifierNamespace.csv'
 
     public static final String DOCSTORE_LOCATION_FALLBACK      = System.getProperty('java.io.tmpdir') + '/laser'   // -> ConfigMapper.getDocumentStorageLocation()
     public static final String DEPLOYBACKUP_LOCATION_FALLBACK  = System.getProperty('java.io.tmpdir')              // -> CustomMigrationCallbacks.onStartMigration()
