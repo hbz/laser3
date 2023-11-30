b5a116188c6dd8939a4068effc2a7f9bbcb887ec
David Klober <david.klober@hbz-nrw.de>
2023-11-30 09:12:00 +0100

    yoda icon

--- grails-app/taglib/de/laser/ContextBarTagLib.groovy
+++ grails-app/taglib/de/laser/ContextBarTagLib.groovy
@@ -106,7 +106,7 @@ class ContextBarTagLib {
 
         if (user.isYoda()) {
             text = 'Systemberechtigung: YODA'
-            icon = 'dungeon'
+            icon = 'star of life'
         }
         else if (user.isAdmin()) {
             text = 'Systemberechtigung: ADMIN'
