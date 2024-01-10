e4c700d2939631c9e5b69a49ec8935422c92ea89
David Klober <david.klober@hbz-nrw.de>
2024-01-10 12:06:15 +0100

    ..

--- grails-app/controllers/de/laser/PackageController.groovy
+++ grails-app/controllers/de/laser/PackageController.groovy
@@ -511,38 +511,38 @@ class PackageController {
 
     /**
      * Call to see planned titles of the package
-     * @return {@link #planned_expired_deleted(grails.web.servlet.mvc.GrailsParameterMap, java.lang.Object)}
+     * @return {@link #planned_expired_deleted(java.lang.String)}
      */
     @DebugInfo(isInstUser_denySupport_or_ROLEADMIN = [])
     @Secured(closure = {
         ctx.contextService.isInstUser_denySupport_or_ROLEADMIN()
     })
     def planned() {
-        planned_expired_deleted(params, "planned")
+        planned_expired_deleted("planned")
     }
 
     /**
      * Call to see expired titles of the package
-     * @return {@link #planned_expired_deleted(grails.web.servlet.mvc.GrailsParameterMap, java.lang.Object)}
+     * @return {@link #planned_expired_deleted(java.lang.String)}
      */
     @DebugInfo(isInstUser_denySupport_or_ROLEADMIN = [])
     @Secured(closure = {
         ctx.contextService.isInstUser_denySupport_or_ROLEADMIN()
     })
     def expired() {
-        planned_expired_deleted(params, "expired")
+        planned_expired_deleted("expired")
     }
 
     /**
      * Call to see deleted titles of the package
-     * @return {@link #planned_expired_deleted(grails.web.servlet.mvc.GrailsParameterMap, java.lang.Object)}
+     * @return {@link #planned_expired_deleted(java.lang.String)}
      */
     @DebugInfo(isInstUser_denySupport_or_ROLEADMIN = [])
     @Secured(closure = {
         ctx.contextService.isInstUser_denySupport_or_ROLEADMIN()
     })
     def deleted() {
-        planned_expired_deleted(params, "deleted")
+        planned_expired_deleted("deleted")
     }
 
     /**
@@ -557,7 +557,7 @@ class PackageController {
     @Secured(closure = {
         ctx.contextService.isInstUser_denySupport_or_ROLEADMIN()
     })
-    def planned_expired_deleted(GrailsParameterMap params, func) {
+    def planned_expired_deleted(String func) {
         log.debug("planned_expired_deleted ${params}");
         Map<String, Object> result = packageService.getResultGenerics(params)
 
@@ -567,7 +567,6 @@ class PackageController {
             redirect action: 'index'
             return
         }
-        result.packageInstance = packageInstance
 
         result.currentTippsCounts = TitleInstancePackagePlatform.executeQuery("select count(*) from TitleInstancePackagePlatform as tipp where tipp.pkg = :pkg and tipp.status = :status", [pkg: packageInstance, status: RDStore.TIPP_STATUS_CURRENT])[0]
         result.plannedTippsCounts = TitleInstancePackagePlatform.executeQuery("select count(*) from TitleInstancePackagePlatform as tipp where tipp.pkg = :pkg and tipp.status = :status", [pkg: packageInstance, status: RDStore.TIPP_STATUS_EXPECTED])[0]
@@ -578,13 +577,13 @@ class PackageController {
         String filename
 
         if (func == "planned") {
-            params.status = RDStore.TIPP_STATUS_EXPECTED.id.toString()
+            params.status = RDStore.TIPP_STATUS_EXPECTED.id
             filename = "${escapeService.escapeString(packageInstance.name + '_' + message(code: 'package.show.nav.planned'))}_${DateUtils.getSDF_noTimeNoPoint().format(new Date())}"
         } else if (func == "expired") {
-            params.status = RDStore.TIPP_STATUS_RETIRED.id.toString()
+            params.status = RDStore.TIPP_STATUS_RETIRED.id
             filename = "${escapeService.escapeString(packageInstance.name + '_' + message(code: 'package.show.nav.expired'))}_${DateUtils.getSDF_noTimeNoPoint().format(new Date())}"
         } else if (func == "deleted") {
-            params.status = RDStore.TIPP_STATUS_DELETED.id.toString()
+            params.status = RDStore.TIPP_STATUS_DELETED.id
             filename = "${escapeService.escapeString(packageInstance.name + '_' + message(code: 'package.show.nav.deleted'))}_${DateUtils.getSDF_noTimeNoPoint().format(new Date())}"
         }
 
