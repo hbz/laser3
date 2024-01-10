9c6aae5ce861765ce3b2bbb13eade8fccb9a232a
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2024-01-10 08:43:13 +0100

    ERMS-5506: Kostenasuschüttung: Wert in EUR und Endpreis in EUR nicht korrekt

--- grails-app/migrations/changelogs/2024-01-09.groovy
+++ grails-app/migrations/changelogs/2024-01-09.groovy
@@ -5,9 +5,9 @@ databaseChangeLog = {
     changeSet(author: "djebeniani (hand-coded)", id: "1704813846277-1") {
         grailsChange {
             change {
-                int updated = sql.executeUpdate("update cost_item set ci_cost_in_local_currency = null, ci_currency_rate = 0.0, ci_last_updated = now() from refdata_value where rdv_id = ci_billing_currency_rv_fk and rdv_value != 'EUR' and (ci_currency_rate = 1 or  ci_currency_rate = 1.0)")
-                confirm("update cost_item set ci_cost_in_local_currency = null, ci_currency_rate = 0.0, ci_last_updated = now() from refdata_value where rdv_id = ci_billing_currency_rv_fk and rdv_value != 'EUR' and (ci_currency_rate = 1 or  ci_currency_rate = 1.0): ${updated}")
-                changeSet.setComments("update cost_item set ci_cost_in_local_currency = null, ci_currency_rate = 0.0, ci_last_updated = now() from refdata_value where rdv_id = ci_billing_currency_rv_fk and rdv_value != 'EUR' and (ci_currency_rate = 1 or  ci_currency_rate = 1.0): ${updated}")
+                int updated = sql.executeUpdate("update cost_item set ci_cost_in_local_currency = null, ci_currency_rate = 0.0, ci_last_updated = now() from refdata_value where rdv_id = ci_billing_currency_rv_fk and rdv_value != 'EUR' and (ci_currency_rate = 1 or  ci_currency_rate = 1.0) and (YEAR(ci_start_date) >= 2023 OR YEAR(ci_date_created) >= 2023) ")
+                confirm("update cost_item set ci_cost_in_local_currency = null, ci_currency_rate = 0.0, ci_last_updated = now() from refdata_value where rdv_id = ci_billing_currency_rv_fk and rdv_value != 'EUR' and (ci_currency_rate = 1 or  ci_currency_rate = 1.0) and (YEAR(ci_start_date) >= 2023 OR YEAR(ci_date_created) >= 2023): ${updated}")
+                changeSet.setComments("update cost_item set ci_cost_in_local_currency = null, ci_currency_rate = 0.0, ci_last_updated = now() from refdata_value where rdv_id = ci_billing_currency_rv_fk and rdv_value != 'EUR' and (ci_currency_rate = 1 or  ci_currency_rate = 1.0) and (YEAR(ci_start_date) >= 2023 OR YEAR(ci_date_created) >= 2023): ${updated}")
             }
             rollback {}
         }
