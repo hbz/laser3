b2bbe73008d69166f914f5905e31d3120f53be5d
galffy <andreas.galffy@hbz-nrw.de>
2024-02-07 12:08:39 +0100

    remarks

--- grails-app/migrations/changelogs/2024-02-07.groovy
+++ grails-app/migrations/changelogs/2024-02-07.groovy
@@ -11,10 +11,11 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1707298648291-2") {
         grailsChange {
             change {
-                sql.execute("update platform set plat_natstat_supplier_id = (select plp_string_value from platform_property join property_definition on plp_type_fk = pd_id where plp_owner_fk = plat_id and pd_name = 'NatStat Supplier ID' order by pd_last_updated desc limit 1)")
-                int deleted = sql.getUpdateCount()
-                confirm("update platform set plat_natstat_supplier_id = (select plp_string_value from platform_property join property_definition on plp_type_fk = pd_id where plp_owner_fk = plat_id and pd_name = 'NatStat Supplier ID' order by pd_last_updated desc limit 1): ${deleted}")
-                changeSet.setComments("update platform set plat_natstat_supplier_id = (select plp_string_value from platform_property join property_definition on plp_type_fk = pd_id where plp_owner_fk = plat_id and pd_name = 'NatStat Supplier ID' order by pd_last_updated desc limit 1): ${deleted}")
+                String query = "update platform set plat_natstat_supplier_id = (select plp_string_value from platform_property join property_definition on plp_type_fk = pd_id where plp_owner_fk = plat_id and pd_name = 'NatStat Supplier ID' and plp_string_value is not null order by pd_last_updated desc limit 1)"
+                sql.execute(query)
+                int updated = sql.getUpdateCount()
+                confirm("${query}: ${updated}")
+                changeSet.setComments("${query}: ${updated}")
             }
             rollback {}
         }
@@ -23,10 +24,11 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1707298648291-3") {
         grailsChange {
             change {
-                sql.execute("delete from platform_property where plp_type_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID')")
+                String query = "delete from platform_property where plp_type_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID')"
+                sql.execute(query)
                 int deleted = sql.getUpdateCount()
-                confirm("delete from platform_property where plp_type_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID'): ${deleted}")
-                changeSet.setComments("delete from platform_property where plp_type_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID'): ${deleted}")
+                confirm("${query}: ${deleted}")
+                changeSet.setComments("${query}: ${deleted}")
             }
             rollback {}
         }
@@ -35,10 +37,11 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1707298648291-4") {
         grailsChange {
             change {
-                sql.execute("delete from property_definition_group_binding where pdgb_property_definition_group_fk in (select pde_id from property_definition_group_item join property_definition on pde_property_definition_fk = pd_id where pd_name = 'NatStat Supplier ID')")
+                String query = "delete from property_definition_group_binding where pdgb_property_definition_group_fk in (select pde_id from property_definition_group_item join property_definition on pde_property_definition_fk = pd_id where pd_name = 'NatStat Supplier ID')"
+                sql.execute(query)
                 int deleted = sql.getUpdateCount()
-                confirm("delete from property_definition_group_binding where pdgb_property_definition_group_fk in (select pde_id from property_definition_group_item join property_definition on pde_property_definition_fk = pd_id where pd_name = 'NatStat Supplier ID'): ${deleted}")
-                changeSet.setComments("delete from property_definition_group_binding where pdgb_property_definition_group_fk in (select pde_id from property_definition_group_item join property_definition on pde_property_definition_fk = pd_id where pd_name = 'NatStat Supplier ID'): ${deleted}")
+                confirm("${query}: ${deleted}")
+                changeSet.setComments("${query}: ${deleted}")
             }
             rollback {}
         }
@@ -47,10 +50,11 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1707298648291-5") {
         grailsChange {
             change {
-                sql.execute("delete from property_definition_group_item where pde_property_definition_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID')")
+                String query = "delete from property_definition_group_item where pde_property_definition_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID')"
+                sql.execute(query)
                 int deleted = sql.getUpdateCount()
-                confirm("delete from property_definition_group_item where pde_property_definition_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID'): ${deleted}")
-                changeSet.setComments("delete from property_definition_group_item where pde_property_definition_fk = (select pd_id from property_definition where pd_name = 'NatStat Supplier ID'): ${deleted}")
+                confirm("${query}: ${deleted}")
+                changeSet.setComments("${query}: ${deleted}")
             }
             rollback {}
         }
@@ -59,10 +63,11 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1707298648291-7") {
         grailsChange {
             change {
-                sql.execute("delete from property_definition where pd_name = 'NatStat Supplier ID'")
+                String query = "delete from property_definition where pd_name = 'NatStat Supplier ID'"
+                sql.execute(query)
                 int deleted = sql.getUpdateCount()
-                confirm("delete from property_definition where pd_name = 'NatStat Supplier ID': ${deleted}")
-                changeSet.setComments("delete from property_definition where pd_name = 'NatStat Supplier ID': ${deleted}")
+                confirm("${query}: ${deleted}")
+                changeSet.setComments("${query}: ${deleted}")
             }
             rollback {}
         }
