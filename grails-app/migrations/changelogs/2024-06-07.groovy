b17e2ee04b5e7cf2e53d6967f447503bbb53d120
galffy <andreas.galffy@hbz-nrw.de>
2024-06-07 15:36:11 +0200

    bug-erms-5739
    as of ERMS-5739, refdata migration

--- grails-app/migrations/changelogs/2024-06-07.groovy
+++ grails-app/migrations/changelogs/2024-06-07.groovy
@@ -5,7 +5,7 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1717761752759-1") {
         grailsChange {
             change {
-                String query = "update person_role set pr_function_type_rv_fk = (select rdv_id from refdata_value where rdv_value = 'Invoicing Contact') where pr_function_type_rv_fk = (select rdv_id from refdata_value where rdv_value = 'Functional Contact Billing Adress')"
+                String query = "update refdata_value set rdv_value = 'Invoicing Contact' where rdv_value = 'Functional Contact Billing Adress'"
                 sql.execute(query)
                 String info = "${query}: ${sql.getUpdateCount()}"
                 confirm(info)
@@ -16,6 +16,7 @@ databaseChangeLog = {
     }
 
     changeSet(author: "galffy (hand-coded)", id: "1717761752759-2") {
+        /*
         grailsChange {
             change {
                 String query = "delete from refdata_value where rdv_value = 'Functional Contact Billing Adress'"
@@ -23,5 +24,6 @@ databaseChangeLog = {
             }
             rollback {}
         }
+        */
     }
 }
