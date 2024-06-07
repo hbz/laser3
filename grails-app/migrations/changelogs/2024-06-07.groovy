f7a0d110df38810972f4529fe2d6235367098862
galffy <andreas.galffy@hbz-nrw.de>
2024-06-07 15:37:41 +0200

    bug-erms-5739
    as of ERMS-5739, refdata migration

--- grails-app/migrations/changelogs/2024-06-07.groovy
+++ grails-app/migrations/changelogs/2024-06-07.groovy
@@ -14,16 +14,4 @@ databaseChangeLog = {
             rollback {}
         }
     }
-
-    changeSet(author: "galffy (hand-coded)", id: "1717761752759-2") {
-        /*
-        grailsChange {
-            change {
-                String query = "delete from refdata_value where rdv_value = 'Functional Contact Billing Adress'"
-                sql.execute(query)
-            }
-            rollback {}
-        }
-        */
-    }
 }
