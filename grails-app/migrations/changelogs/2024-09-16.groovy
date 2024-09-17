ee8d09ba0e36c2567f08c1bacc9d17dc3dd8087d
galffy <andreas.galffy@hbz-nrw.de>
2024-09-17 11:33:10 +0200

    rollback

--- grails-app/migrations/changelogs/2024-09-16.groovy
+++ grails-app/migrations/changelogs/2024-09-16.groovy
@@ -18,19 +18,6 @@ databaseChangeLog = {
     changeSet(author: "galffy (hand-coded)", id: "1726483138229-2") {
         grailsChange {
             change {
-                String query = "delete from survey_org where surorg_address_fk in (select adr_id from address where adr_tenant_fk is null and (adr_provider_fk is not null or adr_vendor_fk is not null))"
-                sql.execute(query)
-                String info = "${query}: ${sql.getUpdateCount()}"
-                confirm(info)
-                changeSet.setComments(info)
-            }
-            rollback {}
-        }
-    }
-
-    changeSet(author: "galffy (hand-coded)", id: "1726483138229-3") {
-        grailsChange {
-            change {
                 String query = "delete from address where adr_tenant_fk is null and (adr_provider_fk is not null or adr_vendor_fk is not null)"
                 sql.execute(query)
                 String info = "${query}: ${sql.getUpdateCount()}"
