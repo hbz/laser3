550c210b19f52a5a03b191194922bab35470178c
David Klober <david.klober@hbz-nrw.de>
2024-11-19 08:21:54 +0100

    erms-6027

--- grails-app/controllers/de/laser/InfoController.groovy
+++ grails-app/controllers/de/laser/InfoController.groovy
@@ -2,12 +2,14 @@ package de.laser
 
 import de.laser.addressbook.Contact
 import de.laser.addressbook.Person
+import de.laser.auth.User
 import de.laser.ctrl.OrganisationControllerService
 import de.laser.remote.ApiSource
 import de.laser.storage.BeanStore
 import de.laser.storage.RDStore
 import de.laser.wekb.TitleInstancePackagePlatform
 import grails.plugin.springsecurity.annotation.Secured
+import grails.web.servlet.mvc.GrailsParameterMap
 
 @Secured(['IS_AUTHENTICATED_FULLY'])
 class InfoController {
@@ -26,29 +28,44 @@ class InfoController {
         }
         else if (params.template == 'reportTitleToProvider') {
 
-            TitleInstancePackagePlatform tipp = TitleInstancePackagePlatform.get(params.id_tipp)
+            result = _reportTitleToProvider(params)
+        }
 
-            ApiSource apiSource = ApiSource.findByTypAndActive(ApiSource.ApiTyp.GOKBAPI, true)
+        render template: '/info/' + params.template, model: result
+    }
 
-            List<Person> ppList = BeanStore.getProviderService().getContactPersonsByFunctionType(
-                    tipp.platform.provider, false, RDStore.PRS_FUNC_SERVICE_SUPPORT
-            )
-            if (!ppList) {
-                ppList = BeanStore.getProviderService().getContactPersonsByFunctionType(
-                        tipp.platform.provider, false, RDStore.PRS_FUNC_GENERAL_CONTACT_PRS)
-            }
-            List<Contact> ccList = ppList.contacts.flatten().findAll { it.contentType?.value in ['E-Mail', 'Mail'] } as List<Contact>
 
-            result = [
-                    mailto : ccList.collect { it.content }.sort().join(','),
-                    mailcc : BeanStore.getContextService().getUser().email
-            ]
-            result.mailSubject = [
-                    de: 'Fehlerhafte Titel-Daten in der We:kb',
-                    en: 'Incorrect title information in the We:kb'
-            ]
-            result.mailText = [
-                    de : """
+    private static Map<String, Object> _reportTitleToProvider(GrailsParameterMap params) {
+
+        Map<String, Object> result = [:]
+
+        ContextService cs  = BeanStore.getContextService()
+        ProviderService ps = BeanStore.getProviderService()
+
+        TitleInstancePackagePlatform tipp = TitleInstancePackagePlatform.get(params.id_tipp)
+        ApiSource apiSource = ApiSource.findByTypAndActive(ApiSource.ApiTyp.GOKBAPI, true)
+
+        List<Person> ppList = ps.getContactPersonsByFunctionType(tipp.platform.provider, false, RDStore.PRS_FUNC_SERVICE_SUPPORT)
+
+        if (!ppList) {
+            ppList = ps.getContactPersonsByFunctionType(tipp.platform.provider, false, RDStore.PRS_FUNC_GENERAL_CONTACT_PRS)
+        }
+        List<Contact> ccList = ppList.contacts.flatten().findAll { it.contentType?.value in ['E-Mail', 'Mail'] } as List<Contact>
+
+        User user = cs.getUser()
+        Org org   = cs.getOrg()
+        CustomerIdentifier custId = CustomerIdentifier.findByCustomerAndPlatform(org, tipp.platform)
+
+        result = [
+                mailto : ccList.collect { it.content }.sort().join(','),
+                mailcc : user.email
+        ]
+        result.mailSubject = [
+                de: 'Fehlerhafte Titel-Daten in der We:kb',
+                en: 'Incorrect title information in the We:kb'
+        ]
+        result.mailText = [
+                de : """
 Sehr geehrte Damen und Herren,
 
 bei einem We:kb-Titel sind mir unvollständige/fehlerhafte Informationen aufgefallen:
@@ -60,9 +77,13 @@ Betroffen ist das folgende Objekt:
 ${tipp.name}
 ${apiSource.baseUrl + '/resource/show/' + tipp.gokbId}
 
-Vielen Dank
+Vielen Dank,
+
+${user.getDisplayName()}
+${org.getSortname()} - ${org.getName()}
+${custId ? ('Kundennummer: ' + custId.value) : ''}
 """,
-                    en : """
+                en : """
 Dear Sir or Madam,
 
 I noticed incomplete/incorrect information in a We:kb title:
@@ -74,11 +95,14 @@ The following object is affected:
 ${tipp.name}
 ${apiSource.baseUrl + '/resource/show/' + tipp.gokbId}
 
-Thank you
-"""
-            ]
-        }
+Thank you,
 
-        render template: '/info/' + params.template, model: result
+${user.getDisplayName()}
+${org.getSortname()} - ${org.getName()}
+${custId ? ('Customer Identifier: ' + custId.value) : ''}
+"""
+        ]
+
+        result
     }
 }
