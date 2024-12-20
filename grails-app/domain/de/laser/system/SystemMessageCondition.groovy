216f7f276937363be01155f11356a812d378d43f
David Klober <david.klober@hbz-nrw.de>
2024-12-20 14:18:28 +0100

    erms-6120 partly

---
+++ grails-app/domain/de/laser/system/SystemMessageCondition.groovy
@@ -0,0 +1,28 @@
+package de.laser.system
+
+class SystemMessageCondition {
+
+    static enum CONFIG {
+        ERMS_6121   ('ERMS_6121', 'Nutzerzahlen überprüfen', SystemMessage.TYPE_DASHBOARD)
+
+        CONFIG(String key, String description, String systemMessageType) {
+            this.key = key
+            this.description = description
+            this.systemMessageType = systemMessageType
+        }
+        public String key
+        public String description
+        public String systemMessageType
+    }
+
+    static mapWith = 'none'
+
+    static boolean isTrue(CONFIG type) {
+        if (type == CONFIG.ERMS_6121) {
+            // do something
+            // true if all conditions are met
+            // false otherwise
+        }
+        false
+    }
+}
