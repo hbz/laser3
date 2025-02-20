a55159002f817313666d1cddf8379d6c38f18f83
David Klober <david.klober@hbz-nrw.de>
2025-02-20 09:00:32 +0100

    asset-pipeline-gradle:4.5.2 / fixed swagger-ui

--- grails-app/assets/javascripts/swagger.js
+++ grails-app/assets/javascripts/swagger.js
@@ -4,6 +4,6 @@ console.log('+ bundle: swagger.js');
 //
 //= require libs/jquery-3.7.1.js
 //
-//= require /swagger-ui/swagger-ui-bundle.js                    //--> assets/vendor
-//= require /swagger-ui/swagger-ui-standalone-preset.js         //--> assets/vendor
+// FAIL@ //= require /swagger-ui/swagger-ui-bundle.js                    //--> assets/vendor
+// FAIL@ //= require /swagger-ui/swagger-ui-standalone-preset.js         //--> assets/vendor
 //= require /crypto-js-4.1.1/crypto-js.js                       //--> assets/vendor
