68953598afc3ffd09e46f232fbfa29aaab0317cd
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2025-04-03 11:09:32 +0200

    ERMS-6362: Merkmal Category A-F ummappen zu Beck: Kategory A-F

--- grails-app/migrations/changelogs/2025-04-03.groovy
+++ grails-app/migrations/changelogs/2025-04-03.groovy
@@ -5,11 +5,8 @@ databaseChangeLog = {
     changeSet(author: "djebeniani (hand-coded)", id: "1743668039192-1") {
         grailsChange {
             change {
-                String query = '''update survey_config_properties set surconpro_survey_property_fk =  (SELECT t.pd_id
-                                                                     FROM property_definition t
-                                                                     WHERE pd_description = 'Survey Property' and pd_name = 'Beck Price Category A-F') where surconpro_survey_property_fk = (SELECT t.pd_id
-                                                                                                                                                                                             FROM property_definition t
-                                                                                                                                                                                             WHERE pd_description = 'Survey Property' and pd_name = 'Category A-F')'''
+                String query = "update survey_config_properties set surconpro_survey_property_fk = (SELECT t.pd_id FROM property_definition t WHERE pd_description = 'Survey Property' and pd_name = 'Beck Price Category A-F') " +
+                        "where surconpro_survey_property_fk = (SELECT t.pd_id FROM property_definition t WHERE pd_description = 'Survey Property' and pd_name = 'Category A-F')"
                 int count = sql.executeUpdate(query)
                 String info = "${query}: ${count}"
                 confirm(info)
@@ -22,11 +19,8 @@ databaseChangeLog = {
     changeSet(author: "djebeniani (hand-coded)", id: "1743668039192-2") {
         grailsChange {
             change {
-                String query = '''update survey_result set surre_type_fk =  (SELECT t.pd_id
-                                                                     FROM property_definition t
-                                                                     WHERE pd_description = 'Survey Property' and pd_name = 'Beck Price Category A-F') where surre_type_fk = (SELECT t.pd_id
-                                                                                                                                                                                             FROM property_definition t
-                                                                                                                                                                                             WHERE pd_description = 'Survey Property' and pd_name = 'Category A-F')'''
+                String query = "update survey_result set surre_type_fk = (SELECT t.pd_id FROM property_definition t WHERE pd_description = 'Survey Property' and pd_name = 'Beck Price Category A-F') " +
+                        "where surre_type_fk = (SELECT t.pd_id FROM property_definition t WHERE pd_description = 'Survey Property' and pd_name = 'Category A-F')"
                 int count = sql.executeUpdate(query)
                 String info = "${query}: ${count}"
                 confirm(info)
