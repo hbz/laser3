9dfea1cb36c4fe36259d98b9076596396ba8c96d
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2025-04-08 19:17:28 +0200

    ERMS-6446: Umfrage /Library Supplier

--- grails-app/domain/de/laser/survey/SurveyInfo.groovy
+++ grails-app/domain/de/laser/survey/SurveyInfo.groovy
@@ -103,19 +103,27 @@ class SurveyInfo {
 
             if(it.subscription)
             {
-                if(!it.pickAndChoose && !(it.surveyProperties.size() > 0)) {
+                if(!it.pickAndChoose && SurveyConfigProperties.countBySurveyConfig()  == 0) {
                     check = false
                 }
             }
 
             if(!it.subscription)
             {
-                if(!(it.surveyProperties.size() > 0)) {
+                if(SurveyConfigProperties.countBySurveyConfig()  == 0) {
                     check = false
                 }
             }
 
-            if(!(it.orgs.org.size() > 0)){
+            if(SurveyOrg.countBySurveyConfig(it) == 0){
+                check = false
+            }
+
+            if(it.vendorSurvey && (SurveyConfigVendor.countBySurveyConfig() == 0)){
+                check = false
+            }
+
+            if(it.packageSurvey && (SurveyConfigPackage.countBySurveyConfig() == 0)){
                 check = false
             }
         }
