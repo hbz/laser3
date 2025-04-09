35134b9de0643b73ad9adb89528e0c0bf9248d25
galffy <andreas.galffy@hbz-nrw.de>
2025-04-09 09:08:07 +0200

    removal of unneeded changeset

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -66,5 +66,4 @@ databaseChangeLog = {
     include file: 'changelogs/2025-03-21.groovy'
     include file: 'changelogs/2025-03-27.groovy'
     include file: 'changelogs/2025-04-03.groovy'
-    include file: 'changelogs/2025-04-09.groovy'
 }
