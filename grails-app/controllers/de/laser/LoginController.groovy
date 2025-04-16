39ba32f553da5277e777a7b2bcda779f699d8620
David Klober <david.klober@hbz-nrw.de>
2025-04-16 07:08:30 +0200

    login/logout controller

--- grails-app/controllers/de/laser/LoginController.groovy
+++ grails-app/controllers/de/laser/LoginController.groovy
@@ -30,27 +30,27 @@ import org.springframework.security.web.savedrequest.HttpSessionRequestCache
 @Secured(['permitAll'])
 class LoginController {
 
-  def authenticationTrustResolver
+    def authenticationTrustResolver
 
     GrailsApplication grailsApplication
     SpringSecurityService springSecurityService
     MailSendService mailSendService
 
-  /**
-   * Default action; redirects to 'defaultTargetUrl' if logged in, /login/auth otherwise.
-   */
-  def index = {
-    if (springSecurityService.isLoggedIn()) {
-      redirect uri: SpringSecurityUtils.securityConfig.successHandler.defaultTargetUrl
+    /**
+    * Default action; redirects to 'defaultTargetUrl' if logged in, /login/auth otherwise.
+    */
+    def index = {
+        if (springSecurityService.isLoggedIn()) {
+            redirect( uri: SpringSecurityUtils.securityConfig.successHandler.defaultTargetUrl )
+        }
+        else {
+            redirect( action: 'auth', params: params )
+        }
     }
-    else {
-      redirect action: 'auth', params: params
-    }
-  }
 
-  /**
-   * Show the login page.
-   */
+    /**
+    * Show the login page.
+    */
     def auth = {
 //        log.debug 'Attempting login ..... ' + request.getRemoteAddr() + ', ' + request.session.id
         log.debug '+ Attempting login ..... ' + request.session.id
@@ -59,7 +59,7 @@ class LoginController {
 
         if (springSecurityService.isLoggedIn()) {
             log.debug '+ Already logged in'
-            redirect uri: config.successHandler.defaultTargetUrl
+            redirect( uri: config.successHandler.defaultTargetUrl )
             return
         }
 
@@ -83,120 +83,123 @@ class LoginController {
         }
         String postUrl = "${request.contextPath}${config.apf.filterProcessesUrl}"
 
-        render view: 'auth', model: [postUrl: postUrl, rememberMeParameter: config.rememberMe.parameter]
+        render( view: 'auth', model: [postUrl: postUrl, rememberMeParameter: config.rememberMe.parameter] )
     }
 
-  /**
-   * The redirect action for Ajax requests.
-   */
-  def authAjax = {
-    response.setHeader 'Location', SpringSecurityUtils.securityConfig.auth.ajaxLoginFormUrl
-    response.sendError HttpStatus.SC_UNAUTHORIZED
-
-    render status: HttpStatus.SC_UNAUTHORIZED, text: HttpStatus.SC_UNAUTHORIZED
-  }
-
-  /**
-   * Show denied page.
-   */
-  def denied = {
-    if (springSecurityService.isLoggedIn() &&
-        authenticationTrustResolver.isRememberMe(SCH.context?.authentication)) {
-      // have cookie but the page is guarded with IS_AUTHENTICATED_FULLY
-      redirect action: 'full', params: params
+    /**
+    * Show denied page.
+    */
+    def denied = {
+        if (springSecurityService.isLoggedIn() &&
+            authenticationTrustResolver.isRememberMe(SCH.context?.authentication)) {
+            // have cookie but the page is guarded with IS_AUTHENTICATED_FULLY
+            redirect( action: 'full', params: params )
+        }
     }
-  }
 
-  /**
-   * Login page for users with a remember-me cookie but accessing a IS_AUTHENTICATED_FULLY page.
-   */
-  def full = {
-    ConfigObject config = SpringSecurityUtils.securityConfig
-    render view: 'auth', params: params,
-      model: [hasCookie: authenticationTrustResolver.isRememberMe(SCH.context?.authentication),
-              postUrl: "${request.contextPath}${config.apf.filterProcessesUrl}"]
-  }
+    /**
+    * Login page for users with a remember-me cookie but accessing a IS_AUTHENTICATED_FULLY page.
+    */
+    def full = {
+        ConfigObject config = SpringSecurityUtils.securityConfig
+        render(
+                view: 'auth', params: params,
+                model: [
+                    hasCookie: authenticationTrustResolver.isRememberMe(SCH.context?.authentication),
+                    postUrl: "${request.contextPath}${config.apf.filterProcessesUrl}"
+                ]
+        )
+    }
 
-  /**
-   * Callback after a failed login. Redirects to the auth page with a warning message.
-   */
-  def authfail = {
+    /**
+    * Callback after a failed login. Redirects to the auth page with a warning message.
+    */
+    def authfail = {
     //def username = session[UsernamePasswordAuthenticationFilter.SPRING_SECURITY_LAST_USERNAME_KEY]
     //def username = session[SpringSecurityUtils.SPRING_SECURITY_LAST_USERNAME_KEY]
+        String msg = ''
+        def exception = session[WebAttributes.AUTHENTICATION_EXCEPTION]
 
-    String msg = ''
-    def exception = session[WebAttributes.AUTHENTICATION_EXCEPTION]
-    if (exception) {
-      if (exception instanceof AccountExpiredException) {
-        msg = g.message(code: 'springSecurity.errors.login.expired')
-      }
-      else if (exception instanceof CredentialsExpiredException) {
-        msg = g.message(code: 'springSecurity.errors.login.passwordExpired')
-      }
-      else if (exception instanceof LockedException) {
-        msg = g.message(code: 'springSecurity.errors.login.locked') + ' ' + g.message(code: 'springSecurity.errors.login.locked.duration')
-      }
-      else if (exception instanceof DisabledException) {
-          msg = g.message(code: 'springSecurity.errors.login.disabled')
-      }
-      else {
-        msg = g.message(code: 'springSecurity.errors.login.fail')
-      }
-    }
-    log.warn '+ Login failed ..... ' + request.getRemoteAddr() + ' - ' + msg
-
-    if (springSecurityService.isAjax(request)) {
-      render([error: msg] as JSON)
-    }
-    else {
-      flash.error = msg
-      redirect action: 'auth', params: params
-    }
-  }
-
-  /**
-   * The Ajax success redirect url.
-   */
-  def ajaxSuccess = {
-    render([success: true, username: springSecurityService.authentication.name] as JSON)
-  }
-
-  /**
-   * The Ajax denied redirect url.
-   */
-  def ajaxDenied = {
-    render([error: 'access denied'] as JSON)
-  }
-
-  /**
-   * Resets for a given username the password. Has to be corrected later.
-   */
-  @Transactional
-  def resetForgottenPassword() {
-    if(!params.forgotten_username) {
-      flash.error = g.message(code:'menu.user.forgottenPassword.userMissing') as String
-    }
-    else {
-      User user = User.findByUsername(params.forgotten_username)
-      if (user) {
-        String newPassword = PasswordUtils.getRandomUserPassword()
-        user.password = newPassword
-        if (user.save()) {
-          flash.message = message(code: 'user.newPassword.successNoOutput') as String
-
-          mailSendService.sendMailToUser(user, message(code: 'email.subject.forgottenPassword'), '/mailTemplates/text/newPassword', [user: user, newPass: newPassword])
+        if (exception) {
+            if (exception instanceof AccountExpiredException) {
+                msg = g.message(code: 'springSecurity.errors.login.expired')
+            }
+            else if (exception instanceof CredentialsExpiredException) {
+                msg = g.message(code: 'springSecurity.errors.login.passwordExpired')
+            }
+            else if (exception instanceof LockedException) {
+                msg = g.message(code: 'springSecurity.errors.login.locked') + ' ' + g.message(code: 'springSecurity.errors.login.locked.duration')
+            }
+            else if (exception instanceof DisabledException) {
+                msg = g.message(code: 'springSecurity.errors.login.disabled')
+            }
+            else {
+                msg = g.message(code: 'springSecurity.errors.login.fail')
+            }
         }
-      }
-      else flash.error = g.message(code:'menu.user.forgottenPassword.userError') as String
+        log.warn '+ Login failed ..... ' + request.getRemoteAddr() + ' - ' + msg
+
+        if (springSecurityService.isAjax(request)) {
+            render( [error: msg] as JSON )
+        }
+        else {
+            flash.error = msg
+            redirect( action: 'auth', params: params )
+        }
     }
-    redirect action: 'auth'
-  }
+
+    /**
+     * The redirect action for Ajax requests.
+     */
+    def authAjax = {
+        response.setHeader( 'Location', SpringSecurityUtils.securityConfig.auth.ajaxLoginFormUrl )
+        response.sendError( HttpStatus.SC_UNAUTHORIZED )
+
+        render( status: HttpStatus.SC_UNAUTHORIZED, text: HttpStatus.SC_UNAUTHORIZED )
+    }
+
+    /**
+    * The Ajax success redirect url.
+    */
+    def ajaxSuccess = {
+        render( [success: true, username: springSecurityService.authentication.name] as JSON )
+    }
+
+    /**
+    * The Ajax denied redirect url.
+    */
+    def ajaxDenied = {
+        render( [error: 'access denied'] as JSON )
+    }
+
+    /**
+    * Resets for a given username the password. Has to be corrected later.
+    */
+    @Transactional
+    def resetForgottenPassword() {
+        if(!params.forgotten_username) {
+            flash.error = g.message(code:'menu.user.forgottenPassword.userMissing') as String
+        }
+        else {
+            User user = User.findByUsername(params.forgotten_username)
+            if (user) {
+                String newPassword = PasswordUtils.getRandomUserPassword()
+                user.password = newPassword
+                if (user.save()) {
+                    flash.message = message(code: 'user.newPassword.successNoOutput') as String
+                    mailSendService.sendMailToUser(user, message(code: 'email.subject.forgottenPassword'), '/mailTemplates/text/newPassword', [user: user, newPass: newPassword])
+                }
+            }
+            else flash.error = g.message(code:'menu.user.forgottenPassword.userError') as String
+        }
+        redirect( action: 'auth' )
+    }
 
     /**
      * Triggers the sending of the forgotten username to the given mail address
      * @return redirects to the login page
      */
-  def getForgottenUsername() {
+    def getForgottenUsername() {
         if(!params.forgotten_username_mail) {
             flash.error = g.message(code:'menu.user.forgottenUsername.userMissing') as String
         }
@@ -210,8 +213,8 @@ class LoginController {
             }
             else flash.error = g.message(code:'menu.user.forgottenUsername.userError') as String
         }
-        redirect action: 'auth'
-  }
+        redirect( action: 'auth' )
+    }
 
     /**
      * Checks if the last requested page before a login warning aims to a valid page
