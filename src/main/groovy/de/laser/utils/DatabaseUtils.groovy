4551938e8eabc9b1ef7d9795a46f3f3dc96a14a3
David Klober <david.klober@hbz-nrw.de>
2025-08-14 09:53:06 +0200

    hql debug helper

--- src/main/groovy/de/laser/utils/DatabaseUtils.groovy
+++ src/main/groovy/de/laser/utils/DatabaseUtils.groovy
@@ -1,6 +1,16 @@
 package de.laser.utils
 
+import de.laser.GlobalService
+import de.laser.storage.BeanStore
 import groovy.util.logging.Slf4j
+import org.grails.core.artefact.DomainClassArtefactHandler
+import org.hibernate.Session
+import org.hibernate.SessionFactory
+import org.hibernate.engine.spi.SessionFactoryImplementor
+import org.hibernate.hql.internal.ast.ASTQueryTranslatorFactory
+import org.hibernate.hql.spi.QueryTranslator
+import org.hibernate.hql.spi.QueryTranslatorFactory
+import org.hibernate.query.Query
 
 /**
  * Helper class for database query related tasks
@@ -64,5 +74,73 @@ class DatabaseUtils {
 
         log.debug('getQueryStruct_ilike(List fields, value) -> ' + query + ' ? ' + val)
         [query: query, name: name, value: val]
+    }
+
+
+    static void debugHQL(String hql, Map params) {
+        SessionFactory sf = BeanStore.get('sessionFactory') as SessionFactory
+        Session session = sf.openSession()
+
+        Query query = session.createQuery(hql)
+        params.each{ key, value -> query.setParameter(key, value) }
+
+        QueryTranslatorFactory translatorFactory = new ASTQueryTranslatorFactory()
+        QueryTranslator translator = translatorFactory.createQueryTranslator(
+                query.getQueryString(),
+                query.getQueryString(),
+                Collections.EMPTY_MAP,
+                sf as SessionFactoryImplementor,
+                null
+        )
+        translator.compile(params, false)
+
+        String sql = translator.getSQLString()
+        String exp = ''
+        try {
+            String tmp = sql
+            translator.collectedParameterSpecifications.eachWithIndex{ ps, i ->
+                Map.Entry tk = translator.tokenReplacements.find{ it.key == ps.name } as Map.Entry
+                tmp = tmp.replaceFirst('\\?', _debugHQL_retoken(tk).toString())
+            }
+            sql = tmp
+
+            exp = GlobalService.obtainSqlConnection().rows('EXPLAIN (COSTS FALSE) ' + sql).collect{ it.toString().replaceFirst('QUERY PLAN=', '') }.join('\n')
+        }
+        catch (Exception e) { println e.getMessage() }
+
+        println '+-------------- hql/sql/explain ---------------'
+        println '-   ' + hql
+        if (params.keySet()) {
+            println '-   ' + ('^ ' * params.keySet().size()) + params.toMapString()
+        }
+        println '-----'
+        println '->  ' + sql
+        println '-----'
+        println exp
+        println '-----'
+    }
+
+    static def _debugHQL_retoken(Map.Entry entry) {
+        println entry
+        def val = entry.value
+
+        if (val == null) {
+            return null
+        }
+        else if (val instanceof List) {
+            return val.collect{ val2 -> _debugHQL_retoken([fake: val2].firstEntry()) }.join(', ')
+        }
+        else if (val instanceof Byte || val instanceof Short || val instanceof Integer || val instanceof Long || val instanceof Float || val instanceof Double) {
+            return val
+        }
+        else if (val instanceof Boolean) {
+            return val
+        }
+        else if (DomainClassArtefactHandler.isDomainClass(val.getClass())) {
+            return val.id
+        }
+        else {
+            return "'${val}'"
+        }
     }
 }
